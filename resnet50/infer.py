cdec024d158f48055af43f5f379804b26ba05d2e
Liang Depeng <liangdepeng@gmail.com>
2021-05-28 16:21:50 +0800

    Dev resnet50 (#2)
    * add rnn demo
    
    * update resnet50

--- resnet50/infer.py
+++ resnet50/infer.py
@@ -1,19 +1,18 @@
-import oneflow as flow
-import oneflow.nn as nn
+import oneflow.experimental as flow
 
 import argparse
 import numpy as np
 import time
-import torch
 
 from models.resnet50 import resnet50
+from torch._C import device
 from utils.imagenet1000_clsidx_to_labels import clsidx_2_labels
 from utils.numpy_data_utils import load_image
 
 def _parse_args():
-    parser = argparse.ArgumentParser("flags for save style transform model")
+    parser = argparse.ArgumentParser("flags for test resnet50")
     parser.add_argument(
-        "--model_path", type=str, default="./resnet50-19c8e357.pth", help="model path"
+        "--model_path", type=str, default="./resnet50_imagenet_pretrain_model", help="model path"
     )
     parser.add_argument(
         "--image_path", type=str, default="", help="input image path"
@@ -26,26 +25,21 @@ def main(args):
 
     start_t = time.time()
     res50_module = resnet50()
-    dic = res50_module.state_dict()
     end_t = time.time()
     print('init time : {}'.format(end_t - start_t))
 
     start_t = time.time()
-    torch_params = torch.load(args.model_path)
-    torch_keys = torch_params.keys()
-
-    for k in dic.keys():
-        if k in torch_keys:
-            dic[k] = torch_params[k].detach().numpy()
-    res50_module.load_state_dict(dic)
+    pretrain_models = flow.load("resnet50_imagenet_pretrain_model")
+    res50_module.load_state_dict(pretrain_models)
     end_t = time.time()
     print('load params time : {}'.format(end_t - start_t))
 
     res50_module.eval()
+    res50_module.to("cuda")
 
     start_t = time.time()
     image = load_image(args.image_path)
-    image = flow.Tensor(image)
+    image = flow.Tensor(image, device=flow.device("cuda"))
     predictions = res50_module(image).softmax()
     predictions = predictions.numpy()
     end_t = time.time()
--- resnet50/train_oneflow.py
+++ resnet50/train_oneflow.py
@@ -1,24 +1,26 @@
 import oneflow.experimental as flow
 
-import numpy as np
-import time
 import argparse
+import numpy as np
 import os
-import torch
+import time
 
 import models.pytorch_resnet50 as pytorch_resnet50
 from models.resnet50 import resnet50
-from utils.imagenet1000_clsidx_to_labels import clsidx_2_labels
 from utils.ofrecord_data_utils import OFRecordDataLoader
 
 def _parse_args():
-    parser = argparse.ArgumentParser("flags for save style transform model")
+    parser = argparse.ArgumentParser("flags for train resnet50")
     parser.add_argument(
-        "--save_checkpoint_path", type=str, default="./checkpoints", help="checkpoint root dir"
+        "--save_checkpoint_path", type=str, default="./checkpoints", help="save checkpoint root dir"
+    )
+    parser.add_argument(
+        "--load_checkpoint", type=str, default="./checkpoints/epoch_xxx_val_acc_xxx", help="load checkpoint"
     )
     parser.add_argument(
         "--dataset_path", type=str, default="./ofrecord", help="dataset path"
     )
+
     return parser.parse_args()
 
 def main(args):
@@ -30,13 +32,13 @@ def main(args):
     val_batch_size = 16
 
     train_data_loader = OFRecordDataLoader(
-                            ofrecord_root = "./ofrecord",
+                            ofrecord_root = args.dataset_path,
                             mode = "train",
                             dataset_size = 9469,
                             batch_size = train_batch_size)
 
     val_data_loader = OFRecordDataLoader(
-                            ofrecord_root = "./ofrecord",
+                            ofrecord_root = args.dataset_path,
                             mode = "val",
                             dataset_size = 3925,
                             batch_size = val_batch_size)
@@ -45,38 +47,12 @@ def main(args):
     learning_rate = 0.001
     mom = 0.9
 
-    ###############################
-    # pytorch init
-    torch_res50_module = pytorch_resnet50.resnet50()
-    start_t = time.time()
-    print(type(start_t))
-    torch_params = torch_res50_module.state_dict()
-    end_t = time.time()
-    print('torch load params time : {}'.format(end_t - start_t))
-    torch_res50_module.to('cuda')
-    torch_sgd = torch.optim.SGD(torch_res50_module.parameters(), lr=learning_rate, momentum=mom)
-    
-    corss_entropy = torch.nn.CrossEntropyLoss()
-    corss_entropy.to('cuda')
-    ###############################
-
     #################
     # oneflow init
     start_t = time.time()
     res50_module = resnet50()
     end_t = time.time()
     print('init time : {}'.format(end_t - start_t))
-
-    start_t = time.time()
-    torch_keys = torch_params.keys()
-
-    dic = res50_module.state_dict()
-    for k in dic.keys():
-        if k in torch_keys:
-            dic[k] = torch_params[k].numpy()
-    res50_module.load_state_dict(dic)
-    end_t = time.time()
-    print('load params time : {}'.format(end_t - start_t))
 
     of_corss_entropy = flow.nn.CrossEntropyLoss()
 
@@ -94,9 +70,9 @@ def main(args):
 
     for epoch in range(epochs):
         res50_module.train()
-        torch_res50_module.train()
 
         for b in range(len(train_data_loader)):
+        # for b in range(10):
             print("epoch %d train iter %d" % (epoch, b))
             image, label = train_data_loader.get_batch()
         
@@ -114,27 +90,12 @@ def main(args):
             of_losses.append(l)
             print('oneflow loss {}, train time : {}'.format(l, end_t - start_t))
 
-            # pytroch train
-            start_t = time.time()
-            image = torch.tensor(image.numpy()).to('cuda')
-            label = torch.tensor(label.numpy(), dtype=torch.long, requires_grad=False).to('cuda')
-            logits = torch_res50_module(image)
-            loss = corss_entropy(logits, label)
-            loss.backward()
-            torch_sgd.step()
-            torch_sgd.zero_grad()
-            end_t = time.time()
-            l = loss.cpu().detach().numpy()
-            torch_losses.append(l)
-            print('pytorch loss {}, train time : {}'.format(l, end_t - start_t))
-
         print("epoch %d train done, start validation" % epoch)
 
         res50_module.eval()
-        torch_res50_module.eval()
         correct_of = 0.0
-        correct_torch = 0.0
         for b in range(len(val_data_loader)):
+        # for b in range(10):
             print("epoch %d val iter %d" % (epoch, b))
             image, label = val_data_loader.get_batch()
 
@@ -153,21 +114,7 @@ def main(args):
             end_t = time.time()
             print("of predict time: %f, %d" % (end_t - start_t, correct_of))
 
-            # pytroch val
-            start_t = time.time()
-            image = torch.from_numpy(image.numpy()).to('cuda')
-            with torch.no_grad():
-                logits = torch_res50_module(image)
-                predictions = logits.softmax(-1)
-            torch_predictions = predictions.cpu().detach().numpy()
-            clsidxs = np.argmax(torch_predictions, axis=1)
-            for i in range(val_batch_size):
-                if clsidxs[i] == label_nd[i]:
-                    correct_torch += 1
-            end_t = time.time()
-            print("torch predict time: %f, %d" % (end_t - start_t, correct_torch))
-
-        print("epoch %d, oneflow top1 val acc: %f, torch top1 val acc: %f" % (epoch, correct_of / all_samples, correct_torch / all_samples))
+        print("epoch %d, oneflow top1 val acc: %f" % (epoch, correct_of / all_samples))
         
         # flow.save(res50_module.state_dict(), os.path.join(args.save_checkpoint_path, "epoch_%d_val_acc_%f" % (epoch, correct_of / all_samples)))
 
@@ -175,13 +122,6 @@ def main(args):
     for o in of_losses:
         writer.write("%f\n" % o)
     writer.close()
-
-    writer = open("torch_losses.txt", "w")
-    for o in torch_losses:
-        writer.write("%f\n" % o)
-    writer.close()
-
-
 
 if __name__ == "__main__":
     args = _parse_args()
