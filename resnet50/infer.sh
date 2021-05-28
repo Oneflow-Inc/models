761b34194afdd0405271dd8c9c8e29149b9b3e6a
Liang Depeng <liangdepeng@gmail.com>
2021-05-28 16:21:50 +0800

    Dev resnet50 (#2)
    * add rnn demo
    
    * update resnet50

---
+++ resnet50/infer.sh
@@ -0,0 +1,11 @@
+PRETRAIN_MODEL_PATH="resnet50_imagenet_pretrain_model"
+IMAGE_PATH="data/fish.jpg"
+# IMAGE_PATH="data/tiger.jpg"
+# IMAGE_PATH="data/ILSVRC2012_val_00020287.JPEG"
+
+if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/classification/resnet50_imagenet_pretrain_model.tar.gz
+  tar zxf resnet50_imagenet_pretrain_model.tar.gz
+fi
+
+python3 infer.py --model_path $PRETRAIN_MODEL_PATH --image_path $IMAGE_PATH
