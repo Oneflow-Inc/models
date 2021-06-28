12e4c1975f99efbf381b224a5dab093ba77024d0
Zhiqiu(Oscar) Xu <oscar.xzq@berkeley.edu>
2021-06-28 02:19:23 -0500

    remove images (#43)

--- alexnet/infer.sh
+++ alexnet/infer.sh
@@ -5,6 +5,11 @@ IMAGE_PATH="data/fish.jpg"
 # IMAGE_PATH="data/tiger.jpg"
 # IMAGE_PATH="data/ILSVRC2012_val_00020287.JPEG"
 
+if [ ! -d "data" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/data.tar.gz
+  tar zxf data.tar.gz
+fi
+
 if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
   wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/classification/alexnet/alexnet_oneflow_model.tar.gz
   tar -xzvf alexnet_oneflow_model.tar.gz
