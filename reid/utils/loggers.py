7e2e5c82190eb71f57a68d5db480b09c14ddeaef
ZhongHW <35329085+puchapu@users.noreply.github.com>
2021-06-30 10:04:40 +0800

    Dev reid (#36)
    * init_reid
    
    * update readme
    
    * modify format&comment
    
    * update format
    
    * add choice of metric args
    
    * update bash file
    
    * update dataset-url
    
    * autopep8
    
    * modify dataset-zip
    
    * modify-datasets

---
+++ reid/utils/loggers.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+
+__all__ = ['Logger', 'RankLogger']
+
+import sys
+import os
+import os.path as osp
+
+
+def mkdir_if_missing(dirname):
+    """Creates dirname if it is missing."""
+    if not osp.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+class Logger(object):
+    """Writes console output to external text file.
+
+    Imported from `<https://github.com/Cysu/open-reid/blob/master/reid/utils/logging.py>`_
+
+    Args:
+        fpath (str): directory to save logging file.
+    """
+
+    def __init__(self, fpath=None):
+        self.console = sys.stdout
+        self.file = None
+        if fpath is not None:
+            mkdir_if_missing(osp.dirname(fpath))
+            self.file = open(fpath, 'w')
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        self.close()
+
+    def write(self, msg):
+        self.console.write(msg)
+        if self.file is not None:
+            self.file.write(msg)
+
+    def flush(self):
+        self.console.flush()
+        if self.file is not None:
+            self.file.flush()
+            os.fsync(self.file.fileno())
+
+    def close(self):
+        self.console.close()
+        if self.file is not None:
+            self.file.close()
