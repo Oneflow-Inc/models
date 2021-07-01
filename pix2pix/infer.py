56f5794332cd2fb5d98781c2f4235f47c8cefa10
Fangwen Wu <hzwufangwen@gmail.com>
2021-07-01 15:30:01 +0800

    add pix2pix (#30)
    * first commit
    
    * update fix2fix
    
    * update pix2pix
    
    * auto format by CI
    
    Co-authored-by: YayeoCddy <Dy_Chen95@163.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

---
+++ pix2pix/infer.py
@@ -0,0 +1,50 @@
+import os
+import oneflow.experimental as flow
+import argparse
+import numpy as np
+import time
+from utils.data_utils import load_image
+from utils.utils import to_numpy, to_tensor, save_images
+from models.networks import Generator
+
+
+def main(args):
+    test_x, test_y = load_image(args.image_path)
+
+    test_inp = to_tensor(test_x.astype(np.float32))
+    test_target = to_tensor(test_y.astype(np.float32))
+
+    generator = Generator().to("cuda")
+
+    start_t = time.time()
+    pretrain_model = flow.load(args.model_path)
+    generator.load_state_dict(pretrain_model)
+    end_t = time.time()
+    print("load params time : {}".format(end_t - start_t))
+
+    start_t = time.time()
+    generator.eval()
+    with flow.no_grad():
+        gout = to_numpy(generator(test_inp), False)
+    end_t = time.time()
+    print("infer time : {}".format(end_t - start_t))
+
+    # save images
+    save_images(
+        gout,
+        test_inp.numpy(),
+        test_target.numpy(),
+        path=os.path.join("./testimage.png"),
+        plot_size=1,
+    )
+
+
+if __name__ == "__main__":
+    flow.enable_eager_execution()
+    parser = argparse.ArgumentParser(description="oneflow PIX2PIX")
+    parser.add_argument("--model_path", type=str, required=True, help="model path")
+    parser.add_argument(
+        "--image_path", type=str, required=True, help="input image path"
+    )
+    args = parser.parse_args()
+    main(args)
