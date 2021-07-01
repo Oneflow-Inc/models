a4e061980247c195c3d6b78be749a337410225c6
Liang Depeng <liangdepeng@gmail.com>
2021-07-01 16:34:47 +0800

    Dev bert implementation (#50)
    * add bert implementation
    
    * bert-align
    
    * revise readme
    
    * revise readme
    
    * loader
    
    * loader,set
    
    * log
    
    * rm
    
    * Update README.md
    
    * auto format by CI
    
    * Update README.md
    
    * pr1
    
    * pr1
    
    * readme
    
    * rm main
    
    Co-authored-by: Flowingsun007 <flowingsun007@163.com>
    Co-authored-by: lichunyou <2695928308@qq.com>
    Co-authored-by: lichunyou <33850693+lcylcy@users.noreply.github.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

---
+++ bert-oneflow/model/utils/layer_norm.py
@@ -0,0 +1,18 @@
+import oneflow.experimental.nn as nn
+import oneflow.experimental as flow
+
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)."
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.eps = eps
+        self.a_2 = nn.Parameter(flow.Tensor(flow.ones(features, dtype=flow.float32)))
+        self.b_2 = nn.Parameter(flow.Tensor(flow.zeros(features, dtype=flow.float32)))
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+
+        std = x.std(dim=-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
--- reid/utils/distance.py
+++ reid/utils/distance.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.spatial.distance import cdist
 
 
-def compute_distance_matrix(input1, input2, metric='euclidean'):
+def compute_distance_matrix(input1, input2, metric="euclidean"):
     """A wrapper function for computing distance matrix.
 
     Args:
@@ -19,13 +19,13 @@ def compute_distance_matrix(input1, input2, metric='euclidean'):
 
     assert input1.shape[1] == input2.shape[1]
 
-    if metric == 'euclidean':
+    if metric == "euclidean":
         distmat = euclidean_squared_distance(input1, input2)
-    elif metric == 'cosine':
+    elif metric == "cosine":
         distmat = cosine_distance(input1, input2)
     else:
         raise ValueError(
-            'Unknown distance metric: {}. '
+            "Unknown distance metric: {}. "
             'Please choose either "euclidean" or "cosine"'.format(metric)
         )
 
@@ -33,12 +33,12 @@ def compute_distance_matrix(input1, input2, metric='euclidean'):
 
 
 def euclidean_squared_distance(input1, input2):
-    dist = cdist(input1, input2, metric='euclidean').astype(np.float16)
+    dist = cdist(input1, input2, metric="euclidean").astype(np.float16)
     distmat = np.power(dist, 2).astype(np.float16)
     return distmat
 
 
 def cosine_distance(input1, input2):
-    dist = cdist(input1, input2, metric='cosine').astype(np.float16)
+    dist = cdist(input1, input2, metric="cosine").astype(np.float16)
     distmat = np.power(dist, 2).astype(np.float16)
     return distmat
