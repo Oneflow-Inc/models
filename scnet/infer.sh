33914fb38f051f77352a2af3dd509e35c70133f3
XinYangDong <49740442+XinYangDong@users.noreply.github.com>
2021-07-19 10:42:53 +0800

    Update infer.sh

--- scnet/infer.sh
+++ scnet/infer.sh
@@ -1,13 +1,6 @@
 set -aux
 
-PRETRAIN_MODEL_PATH="resnet50_imagenet_pretrain_model"
-IMAGE_PATH="data/fish.jpg"
-# IMAGE_PATH="data/tiger.jpg"
-# IMAGE_PATH="data/ILSVRC2012_val_00020287.JPEG"
-
-if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
-  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/classification/resnet50_imagenet_pretrain_model.tar.gz
-  tar zxf resnet50_imagenet_pretrain_model.tar.gz
-fi
+PRETRAIN_MODEL_PATH="scnet_acc_0.947254"
+IMAGE_PATH="data/img_red.png"
 
 python3 infer.py --model_path $PRETRAIN_MODEL_PATH --image_path $IMAGE_PATH
