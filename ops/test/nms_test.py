ad769b1bf7aa487a981ba49b66b8bc9d6b48c6e4
simonJJJ <821898965@qq.com>
2021-07-26 11:58:47 +0800

    fix nms

--- ops/test/nms_test.py
+++ ops/test/nms_test.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import numpy as np
 
 import oneflow.experimental as flow
-from oneflow.python.test.modules.test_util import GenArgList
+from ops.test.test_util import GenArgList
 from ops import nms, lib_path
 
 flow.config.load_library_now(lib_path())
