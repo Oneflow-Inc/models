6d8bfc84ad3f01f4da8ce78d0603816e51d98b47
XinYangDong <49740442+XinYangDong@users.noreply.github.com>
2021-07-28 11:11:35 +0800

    Update train_oneflow.sh

--- scnet/train_oneflow.sh
+++ scnet/train_oneflow.sh
@@ -2,6 +2,11 @@ set -aux
 
 OFRECORD_PATH="./ofrecord/hustcolor"
 
+if [ ! -d "$OFRECORD_PATH" ]; then
+    wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/datasets/models/scnet/ofrecord.zip
+    unzip ofrecord.zip
+fi
+
 CHECKPOINT_PATH="checkpoints"
 if [ ! -d "$CHECKPOINT_PATH" ]; then
     mkdir $CHECKPOINT_PATH
