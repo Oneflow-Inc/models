b012a1577882a6f0a92e7cd8a6eff62975cf099d
XinYangDong <49740442+XinYangDong@users.noreply.github.com>
2021-07-27 19:17:45 -0800

    add scnet (#63)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update infer.sh
    
    * Update train_oneflow.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Delete plot.py
    
    * Delete ofrecord_from_string.py
    
    删除ofrecord分析脚本
    
    * Update README.md
    
    修改了相关描述
    
    * Delete scnet/data directory
    
    * Update train_oneflow.py
    
    * Update infer.py
    
    import oneflow as flow
    
    * Update resnet50.py
    
    import oneflow as flow
    
    * Update resnet50.py
    
    * Update scloss.py
    
    * Update ofrecord_data_utils.py
    
    import oneflow as flow
    
    * Update README.md
    
    * Update infer.sh
    
    * Update train_oneflow.sh
    
    * Update infer.sh

---
+++ scnet/infer.sh
@@ -0,0 +1,11 @@
+set -aux
+
+PRETRAIN_MODEL_PATH="scnet_acc_0.947254"
+IMAGE_PATH="data/img_red.png"
+
+if [ ! -f "$IMAGE_PATH" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/datasets/models/scnet/data.zip
+  unzip data.zip
+fi
+
+python3 infer.py --model_path $PRETRAIN_MODEL_PATH --image_path $IMAGE_PATH
