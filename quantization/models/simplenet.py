ded0a92922f051397e5a58e5571998e7ab9f729d
BBuf <1182563586@qq.com>
2021-08-04 10:49:51 +0800

    add quantization_aware_training scripts

--- quantization/models/simplenet.py
+++ quantization/models/simplenet.py
@@ -4,18 +4,21 @@ from ..ops import *
 
 class SimpleNet(nn.Module):
 
-    def __init__(self, num_channels=1):
+    def __init__(self, num_channels=3):
         super(SimpleNet, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 40, 3, 1)
-        self.conv2 = nn.Conv2d(40, 40, 3, 1, groups=20)
-        self.fc = nn.Linear(5*5*40, 10)
+        self.conv1 = nn.Conv2d(num_channels, 6, 3, 1, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1, 1)
+        self.conv3 = nn.Conv2d(16, 120, 3, 1, 1)
+        self.fc = nn.Linear(4*4*120, 10)
 
     def forward(self, x):
         x = flow.F.relu(self.conv1(x))
         x = flow.F.max_pool_2d(x, 2, 2)
         x = flow.F.relu(self.conv2(x))
         x = flow.F.max_pool_2d(x, 2, 2)
-        x = x.view(-1, 5*5*40)
+        x = flow.F.relu(self.conv3(x))
+        x = flow.F.max_pool_2d(x, 2, 2)
+        x = x.view(-1, 4*4*120)
         x = self.fc(x)
         return x
 
@@ -23,7 +26,7 @@ class SimpleNet(nn.Module):
         self.qconv1 = q_conv(self.conv1, qi=True, qo=True, quantization_bit=quantization_bit, quantization_scheme=quantization_scheme, quantization_formula=quantization_formula, per_layer_quantization=per_layer_quantization)
         self.qrelu1 = q_relu()
         self.qmaxpool2d_1 = q_max_pool_2d(kernel_size=2, stride=2, padding=0)
-        self.qconv2 = q_conv(self.conv2, qi=False, qo=True, quantization_bit=quantization_bit)
+        self.qconv2 = q_conv(self.conv2, qi=False, qo=True, quantization_bit=quantization_bit, quantization_scheme=quantization_scheme, quantization_formula=quantization_formula, per_layer_quantization=per_layer_quantization)
         self.qrelu2 = q_relu()
         self.qmaxpool2d_2 = q_max_pool_2d(kernel_size=2, stride=2, padding=0)
         self.qfc = q_linear(self.fc, qi=False, qo=True, quantization_bit=quantization_bit, quantization_scheme=quantization_scheme, quantization_formula=quantization_formula, per_layer_quantization=per_layer_quantization)
@@ -35,7 +38,7 @@ class SimpleNet(nn.Module):
         x = self.qconv2(x)
         x = self.qrelu2(x)
         x = self.qmaxpool2d_2(x)
-        x = x.view(-1, 5*5*40)
+        x = x.view(-1, 4*4*120)
         x = self.qfc(x)
         return x
 
@@ -56,7 +59,7 @@ class SimpleNet(nn.Module):
         qx = self.qconv2.quantize_inference(qx)
         qx = self.qrelu2.quantize_inference(qx)
         qx = self.qmaxpool2d_2.quantize_inference(qx)
-        qx = qx.view(-1, 5*5*40)
+        qx = qx.view(-1, 4*4*120)
         qx = self.qfc.quantize_inference(qx)
         out = self.qfc.qo.dequantize_tensor(qx)
         return out
