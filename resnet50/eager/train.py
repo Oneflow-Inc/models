013b46eb69a41c11bc122ba54b3993a231b39769
CPFLAME <410070869@qq.com>
2021-08-06 17:49:12 +0800

    fix bug: change model -> models

--- resnet50/eager/train.py
+++ resnet50/eager/train.py
@@ -7,7 +7,7 @@ import shutil
 from tqdm import tqdm
 import oneflow as flow
 
-from model.resnet50 import resnet50
+from models.resnet50 import resnet50
 from utils.ofrecord_data_utils import OFRecordDataLoader
 
 class AverageMeter:
