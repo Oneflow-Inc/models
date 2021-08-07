8b49c579a5ee6b454cc4e777f8b31e4b0913a9dd
ShawnXuan <xiexuanx2@gmail.com>
2021-08-07 22:18:54 +0800

    first loss OK

--- wide_and_deep/train.py
+++ wide_and_deep/train.py
@@ -8,6 +8,7 @@ import oneflow as flow
 from config import get_args
 from dataloader_utils import OFRecordDataLoader
 from wide_and_deep_module import WideAndDeep
+from util import dump_to_npy, save_param_npy
 
 
 if __name__ == '__main__':
@@ -16,18 +17,18 @@ if __name__ == '__main__':
     args = get_args()
 
     train_dataloader = OFRecordDataLoader(args, data_root=args.data_dir)
-    val_dataloader = OFRecordDataLoader(args, data_root=args.data_dir, mode="val")
+    val_dataloader = OFRecordDataLoader(
+        args, data_root=args.data_dir, mode="val")
     wdl_module = WideAndDeep(args)
     if args.model_load_dir != "":
         print("load checkpointed model from ", args.model_load_dir)
         wdl_module.load_state_dict(flow.load(args.model_load_dir))
-    for name, param in wdl_module.named_parameters():
-        print(name, param.shape)
 
     if args.save_initial_model and args.model_save_dir != "":
         path = os.path.join(args.model_save_dir, 'initial_checkpoint')
         if not os.path.isdir(path):
             flow.save(wdl_module.state_dict(), path)
+    save_param_npy(wdl_module)
 
     bce_loss = flow.nn.BCELoss(reduction="none")
 
@@ -42,6 +43,10 @@ if __name__ == '__main__':
     wdl_module.train()
     for i in range(args.max_iter):
         labels, dense_fields, wide_sparse_fields, deep_sparse_fields = train_dataloader()
+        dump_to_npy(labels, sub=i)
+        dump_to_npy(dense_fields, sub=i)
+        dump_to_npy(wide_sparse_fields, sub=i)
+        dump_to_npy(deep_sparse_fields, sub=i)
         labels = labels.to("cuda").to(dtype=flow.float32)
         dense_fields = dense_fields.to("cuda")
         wide_sparse_fields = wide_sparse_fields.to("cuda")
@@ -49,11 +54,15 @@ if __name__ == '__main__':
         predicts = wdl_module(
             dense_fields, wide_sparse_fields, deep_sparse_fields)
         loss = bce_loss(predicts, labels)
+        dump_to_npy(predicts, sub=i)
+        dump_to_npy(loss, sub=i)
         losses.append(loss.numpy().mean())
         loss.backward(flow.ones_like(loss))
         of_sgd.step()
         of_sgd.zero_grad()
-        if i % args.print_interval == 0:
+        # print(deep_sparse_fields)
+        # print(loss)
+        if (i+1) % args.print_interval == 0:
             l = sum(losses) / len(losses)
             print(f"iter {i} train_loss {l} time {time.time()}")
             if args.eval_batchs <= 0:
@@ -66,7 +75,7 @@ if __name__ == '__main__':
             for j in range(args.eval_batchs):
                 labels, dense_fields, wide_sparse_fields, deep_sparse_fields = val_dataloader()
                 labels = labels.to("cuda").to(dtype=flow.float32)
-                print(labels.numpy().flatten())
+                # print(labels.numpy().flatten())
                 # print(j)
                 dense_fields = dense_fields.to("cuda")
                 wide_sparse_fields = wide_sparse_fields.to("cuda")
@@ -82,9 +91,9 @@ if __name__ == '__main__':
             print(all_labels.shape, all_predicts.shape)
             print(np.isnan(all_predicts).any())
             print(all_labels.flatten())
-            auc = "NaN" if np.isnan(all_predicts).any() else roc_auc_score(all_labels, all_predicts)
+            auc = "NaN" if np.isnan(all_predicts).any(
+            ) else roc_auc_score(all_labels, all_predicts)
             print(f"iter {i} eval_loss {eval_loss/args.eval_batchs} auc {auc}")
 
             losses = []
             wdl_module.train()
-
