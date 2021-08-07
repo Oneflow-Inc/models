f76a4627d0402508f4760cd4baed744779afa196
ShawnXuan <xiexuanx2@gmail.com>
2021-08-07 22:18:15 +0800

    modify train parameters

--- wide_and_deep/train.sh
+++ wide_and_deep/train.sh
@@ -3,9 +3,11 @@ rm core.*
 # export CUDA_VISIBLE_DEVICES=3
 
 python3 train.py \
-    --model_load_dir ./checkpoints/initial_checkpoint \
-    --print_interval 1 \
-    --max_iter 1000
-
-    # --model_save_dir ./checkpoints \
+    --model_load_dir ./checkpoints/merged_checkpoint \
+    --model_save_dir ./checkpoints \
+    --print_interval 10 \
+    --deep_dropout_rate 0 \
+    --max_iter 1
     # --save_initial_model \
+    # --model_load_dir ./checkpoints/initial_checkpoint \
+    # --model_load_dir /home/xiexuan/sandbox/OneFlow-Benchmark/ClickThroughRate/WideDeepLearning/baseline_checkpoint \
