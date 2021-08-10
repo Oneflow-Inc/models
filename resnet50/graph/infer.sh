e0a93529be574992b8412200ed7936d931869d7d
CPFLAME <410070869@qq.com>
2021-08-10 19:01:26 +0800

    infer.sh: del commands

--- resnet50/graph/infer.sh
+++ resnet50/graph/infer.sh
@@ -2,8 +2,6 @@ set -aux
 
 PRETRAIN_MODEL_PATH="resnet50_imagenet_pretrain_model"
 IMAGE_PATH="data/fish.jpg"
-# IMAGE_PATH="data/tiger.jpg"
-# IMAGE_PATH="data/ILSVRC2012_val_00020287.JPEG"
 
 if [ ! -d "data" ]; then
   wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/data.tar.gz
