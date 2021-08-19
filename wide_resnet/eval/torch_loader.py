de7a2257f22aa36d210809abb3e3527a94882bc6
rentainhe <596106517@qq.com>
2021-08-19 13:51:49 +0800

    build wide_resnet model and compare the acc with torch

---
+++ wide_resnet/eval/torch_loader.py
@@ -0,0 +1,99 @@
+import os
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10, CIFAR100, ImageFolder
+from torchvision.transforms import transforms
+
+
+__all__ = ['CIFAR10DataLoader', 'ImageNetDataLoader', 'CIFAR100DataLoader']
+
+
+class CIFAR10DataLoader(DataLoader):
+    def __init__(self, data_dir, split='train', image_size=224, batch_size=16, num_workers=8):
+        if split == 'train':
+            train = True
+            transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            train = False
+            transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+
+        self.dataset = CIFAR10(root=data_dir, train=train, transform=transform, download=True)
+
+        super(CIFAR10DataLoader, self).__init__(
+            dataset=self.dataset,
+            batch_size=batch_size,
+            shuffle=False if not train else True,
+            num_workers=num_workers)
+
+
+class CIFAR100DataLoader(DataLoader):
+    def __init__(self, data_dir, split='train', image_size=224, batch_size=16, num_workers=8):
+        if split == 'train':
+            train = True
+            transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            train = False
+            transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+
+        self.dataset = CIFAR100(root=data_dir, train=train, transform=transform, download=True)
+
+        super(CIFAR100DataLoader, self).__init__(
+            dataset=self.dataset,
+            batch_size=batch_size,
+            shuffle=False if not train else True,
+            num_workers=num_workers)
+
+
+class ImageNetDataLoader(DataLoader):
+    def __init__(self, data_dir, split='train', image_size=224, batch_size=16, num_workers=8):
+
+        if split == 'train':
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.ToTensor(),
+                # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+
+        self.dataset = ImageFolder(root=os.path.join(data_dir, split), transform=transform)
+        super(ImageNetDataLoader, self).__init__(
+            dataset=self.dataset,
+            batch_size=batch_size,
+            shuffle=True if split == 'train' else False,
+            num_workers=num_workers)
+
+
+if __name__ == '__main__':
+    data_loader = ImageNetDataLoader(
+        data_dir='/data/imagenet/extract',
+        split='val',
+        image_size=384,
+        batch_size=16,
+        num_workers=8)
+
+    for images, targets in data_loader:
+        print(targets)
---
+++ wide_resnet/eval/valid_torch.py
@@ -0,0 +1,89 @@
+import os
+import torch
+import oneflow as flow
+import numpy as np
+from tqdm import tqdm
+from torchvision.models import wide_resnet50_2, wide_resnet101_2
+from torch_loader import *
+
+def setup_device(n_gpu_use):
+    n_gpu = torch.cuda.device_count()
+    if n_gpu_use > 0 and n_gpu == 0:
+        print("Warning: There\'s no GPU available on this machine, training will be performed on CPU.")
+        n_gpu_use = 0
+    if n_gpu_use > n_gpu:
+        print("Warning: The number of GPU\'s configured to use is {}, but only {} are available on this machine.".format(n_gpu_use, n_gpu))
+        n_gpu_use = n_gpu
+    device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
+    list_ids = list(range(n_gpu_use))
+    return device, list_ids
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].contiguous().view(-1).float().sum(0)
+        res.append(correct_k / batch_size * 100.0)
+    return res
+
+def main(model, checkpoint_path):
+
+
+    # device
+    device, device_ids = setup_device(1)
+
+    # load checkpoint
+    if checkpoint_path:
+        state_dict = torch.load(checkpoint_path)
+        model.load_state_dict(state_dict)
+        print("Load pretrained weights from {}".format(checkpoint_path))
+
+    # send model to device
+    model = model.to(device)
+    if len(device_ids) > 1:
+        model = torch.nn.DataParallel(model, device_ids=device_ids)
+
+    # create dataloader
+    data_loader = eval("{}DataLoader".format("ImageNet"))(
+                    data_dir="/data/imagenet/extract",
+                    image_size=224,
+                    batch_size=32,
+                    num_workers=8,
+                    split='val')
+    total_batch = len(data_loader)
+
+    # starting evaluation
+    print("Starting evaluation")
+    acc1s = []
+    acc5s = []
+    model.eval()
+    with torch.no_grad():
+        pbar = tqdm(enumerate(data_loader), total=total_batch)
+        for batch_idx, (data, target) in pbar:
+            pbar.set_description("Batch {:05d}/{:05d}".format(batch_idx, total_batch))
+
+            data = data.to(device)
+            target = target.to(device)
+
+            pred_logits = model(data)
+            acc1, acc5 = accuracy(pred_logits, target, topk=(1, 5))
+
+            acc1s.append(acc1.item())
+            acc5s.append(acc5.item())
+
+            pbar.set_postfix(acc1=acc1.item(), acc5=acc5.item())
+
+    print("Evaluation of model {:s} on dataset {:s}, Acc@1: {:.4f}, Acc@5: {:.4f}".format("wide_resnet50_2", "ImageNet", np.mean(acc1s), np.mean(acc5s)))
+
+if __name__ == "__main__":
+    wide_resnet50_2_weight_path = "/data/rentianhe/code/new_models/models/wide_resnet/weight/torch/wide_resnet50_2-95faca4d.pth"
+    wide_resnet101_2_weight_path = "/data/rentianhe/code/new_models/models/wide_resnet/weight/torch/wide_resnet101_2-32ee1156.pth"
+    model = wide_resnet50_2()
+    main(model, wide_resnet50_2_weight_path)
