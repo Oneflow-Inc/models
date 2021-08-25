dd433110484230441d8614f66b06edb0847dd008
WangYizhang01 <37441507+WangYizhang01@users.noreply.github.com>
2021-08-25 14:27:06 +0800

    add SincNet (#124)
    * add SincNet
    
    * auto format by CI
    
    * upload data_lists
    
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

---
+++ SincNet/infer.sh
@@ -0,0 +1,14 @@
+set -aux
+
+
+OPTIONS_PATH='cfg/SincNet_TIMIT.cfg'
+PRETRAIN_MODELS='pretrain_models'
+
+if [ ! -d "$PRETRAIN_MODELS" ]; then
+    wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/audio/SincNet.zip
+    unzip SincNet.zip
+    mv SincNet $PRETRAIN_MODELS 
+    rm -fr SincNet.zip
+fi
+
+python infer.py --cfg $OPTIONS_PATH
