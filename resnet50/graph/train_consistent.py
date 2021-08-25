4b51f347c05f0e3d829d8c581746a77a907ce38f
leaves-zwx <kunta0932@gmail.com>
2021-08-25 20:38:48 +0800

    tidy code

--- resnet50/graph/train_consistent.py
+++ resnet50/graph/train_consistent.py
@@ -1,15 +1,24 @@
+
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
+
+import argparse
+import numpy as np
+import time
+import math
+
+import oneflow as flow
 from oneflow.nn.module import Module
+
 from utils.debug import dump_to_npy
 from utils.ofrecord_data_utils import OFRecordDataLoader
 from models.resnet50 import resnet50
-import oneflow as flow
-import argparse
-import numpy as np
-import os
-import time
 
-import sys
-sys.path.append(".")
+_GLOBAL_VARS = None
 
 
 def parse_args():
@@ -130,54 +139,55 @@ def parse_args():
     return parser.parse_args()
 
 
+class GlobalVars(object):
+    def __init__(self, args):
+        self.rank = flow.distributed.get_rank()
+        self.world_size = flow.distributed.get_world_size()
+        self.total_train_batch_size = args.train_batch_size_per_device * self.world_size
+        self.total_val_batch_size = args.val_batch_size_per_device * self.world_size
+        self.batches_per_epoch = math.ceil(args.train_examples_num // self.total_train_batch_size)
+        self.val_batches_per_epoch = int(args.val_examples_num / self.total_val_batch_size)
+        self.warmup_batches = self.batches_per_epoch * args.warmup_epochs
+        self.num_train_batches = self.batches_per_epoch * args.num_epochs
+        self.decay_batches = self.num_train_batches - self.warmup_batches  # TODO: remove warmup_batches
+
+
+def global_vars():
+    global _GLOBAL_VARS
+    assert _GLOBAL_VARS is not None
+    return _GLOBAL_VARS
+
+
 class LabelSmoothLoss(Module):
     def __init__(self, num_classes=-1, smooth_rate=0.0) -> None:
         super().__init__()
         self.num_classes = num_classes
         self.smooth_rate = smooth_rate
+        self.on_value = 1 - self.smooth_rate + self.smooth_rate / self.num_classes
+        self.off_value = self.smooth_rate / self.num_classes
 
     def forward(self, input, label):
-        onehot_label = flow.F.one_hot(label, num_classes=self.num_classes,
-                                      on_value=1-self.smooth_rate + self.smooth_rate / self.num_classes,
-                                      off_value=self.smooth_rate/self.num_classes)
+        onehot_label = flow.F.one_hot(label, self.num_classes, self.on_value, self.off_value)
         # log_prob = input.softmax(dim=-1).log()
         # onehot_label = flow.F.cast(onehot_label, log_prob.dtype)
         # loss = flow.mul(log_prob * -1, onehot_label).sum(dim=-1).mean()
         loss = flow.F.softmax_cross_entropy(input, onehot_label.to(dtype=input.dtype))
         return loss.mean()
 
-# class SparseCrossEntropyLoss(Module):
-#     def __init__(self, num_classes=-1) -> None:
-#         self.num_classes = num_classes
-#         super().__init__()
-
-#     def forward(self, input, label):
-#         onehot_label = flow.F.one_hot(label, num_classes=self.num_classes)        
-#         loss = flow.F.sparse_softmax_cross_entropy(input, onehot_label, depth=input.shape[-1])
-#         return loss.mean()
-
 
 def prepare_modules(args, to_consistent=True):
-    rank = flow.distributed.get_rank()
-    world_size = flow.distributed.get_world_size()
+    vars = global_vars()
 
     device_list = [i for i in range(args.process_num_per_node)]
     placement = flow.placement("cpu", {0: device_list}) if to_consistent else None
     sbp = [flow.sbp.split(0)] if to_consistent else None
 
-    total_train_batch_size = args.train_batch_size_per_device * world_size
-    total_val_batch_size = args.val_batch_size_per_device * world_size
-
-    batches_per_epoch = args.train_examples_num // total_train_batch_size
-    warmup_batches = batches_per_epoch * args.warmup_epochs
-    num_train_batches = batches_per_epoch * args.num_epochs
-    decay_batches = num_train_batches - warmup_batches
     train_data_loader = OFRecordDataLoader(
         ofrecord_root=args.ofrecord_path,
         mode="validation",  # "train",
         dataset_size=args.train_examples_num,
-        batch_size=total_train_batch_size,
-        total_batch_size=total_train_batch_size,
+        batch_size=vars.total_train_batch_size,
+        total_batch_size=vars.total_train_batch_size,
         ofrecord_part_num=args.ofrecord_part_num,
         placement=placement,
         sbp=sbp,
@@ -187,8 +197,8 @@ def prepare_modules(args, to_consistent=True):
         ofrecord_root=args.ofrecord_path,
         mode="validation",
         dataset_size=args.val_examples_num,
-        batch_size=total_val_batch_size,
-        total_batch_size=total_val_batch_size,
+        batch_size=vars.total_val_batch_size,
+        total_batch_size=vars.total_val_batch_size,
         ofrecord_part_num=args.ofrecord_part_num,
         placement=placement,
         sbp=sbp,
@@ -211,7 +221,7 @@ def prepare_modules(args, to_consistent=True):
             loaded_state_dict = flow.load(
                 args.load_checkpoint, consistent_src_rank=0 if to_consistent else None
             )
-            print("rank %d load_checkpoint >>>>>>>>> " % rank, args.load_checkpoint)
+            print("rank %d load_checkpoint >>>>>>>>> " % vars.rank, args.load_checkpoint)
             resnet50_module.load_state_dict(loaded_state_dict)
 
     if to_consistent:
@@ -224,6 +234,7 @@ def prepare_modules(args, to_consistent=True):
         resnet50_module.to("cuda")
         of_cross_entropy.to("cuda")
         load_ckpt()
+
     print(args)
 
     # flow.save(resnet50_module.state_dict(), "init_ckpt", consistent_dst_rank=0)
@@ -237,51 +248,54 @@ def prepare_modules(args, to_consistent=True):
     #     print(name)
     # print('*'*100)
     # exit()
-    opt = flow.optim.SGD(
-        resnet50_module.parameters(), lr=args.learning_rate, momentum=args.momentum
+
+    optimizer = flow.optim.SGD(
+        resnet50_module.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=3.05175781e-05
     )
 
-    # cosine_annealing_lr = flow.optim.lr_scheduler.CosineAnnealingLR(
-    #     opt, steps=decay_batches
-    # )
-    # if args.warmup_epochs > 0:
-    #     cosine_annealing_lr = flow.optim.lr_scheduler.LinearWarmupLR(
-    #         cosine_annealing_lr, steps=warmup_batches, start_multiplier=0
-    #     )
-    return train_data_loader, val_data_loader, resnet50_module, opt, of_cross_entropy, None#cosine_annealing_lr
+    lr_scheduler = flow.optim.lr_scheduler.CosineDecayLR(
+        optimizer, decay_steps=vars.decay_batches
+    )
+
+    if args.warmup_epochs > 0:
+        lr_scheduler = flow.optim.lr_scheduler.WarmUpLR(
+            lr_scheduler, warmup_factor=0, warmup_iters=vars.warmup_batches, warmup_method="linear"
+        )
+
+    return train_data_loader, val_data_loader, resnet50_module, of_cross_entropy, optimizer, lr_scheduler
 
 
 def main(args):
-    rank = flow.distributed.get_rank()
-    train_data_loader, val_data_loader, resnet50_module, opt, of_cross_entropy, cosine_annealing_lr = prepare_modules(args)
-    
     flow.boxing.nccl.set_fusion_threshold_mbytes(args.nccl_fusion_threshold_mb)
     flow.boxing.nccl.set_fusion_max_ops_num(args.nccl_fusion_max_ops)
     if args.use_fp16 and args.num_nodes * args.process_num_per_node > 1:
         flow.boxing.nccl.enable_use_buffer_to_fuse_all_reduce(False)
 
+    vars = global_vars()
+
+    train_data_loader, val_data_loader, resnet50_module, cross_entropy_loss, optimizer, lr_scheduler = prepare_modules(args)
+
     class Resnet50Graph(flow.nn.Graph):
         def __init__(self):
             super().__init__()
-            self.train_data_loader = train_data_loader
-            self.resnet50 = resnet50_module
-            self.cross_entropy = of_cross_entropy
-            self.add_optimizer(opt)#, lr_sch=cosine_annealing_lr)
-
             if args.use_fp16:
                 self.config.enable_amp(True)
-                loss_scale = flow.nn.graph.amp.DynamicLossScalePolicy(increment_period=2000)
-                self.config.amp_add_loss_scale_policy(loss_scale)
+                grad_scaler = flow.amp.GradScaler(
+                    init_scale=2 ** 30,
+                    growth_factor=2.0,
+                    backoff_factor=0.5,
+                    growth_interval=2000,
+                )
+                self.set_grad_scaler(grad_scaler)
 
             self.config.allow_fuse_add_to_output(True)
-            # self.config.prune_parallel_cast_ops(True)
-            # self.config.cudnn_conv_heuristic_search_algo(False)
-            # self.config.enable_inplace(True)
-            # if args.num_nodes > 1:
-            #     self.config.cudnn_conv_heuristic_search_algo(True)
-            # else:
-            #     self.config.cudnn_conv_heuristic_search_algo(False)
             self.config.allow_fuse_model_update_ops(True)
+
+            self.train_data_loader = train_data_loader
+            self.resnet50 = resnet50_module
+            self.cross_entropy = cross_entropy_loss
+            self.add_optimizer(optimizer)
+            # self.add_optimizer(optimizer, lr_sch=lr_scheduler)
 
         def build(self):
             image, label = self.train_data_loader()
@@ -289,8 +303,9 @@ def main(args):
             label = label.to("cuda")
             logits = self.resnet50(image)
             loss = self.cross_entropy(logits, label)
+            predictions = logits.softmax()
             loss.backward()
-            return loss, image, label, logits
+            return loss, predictions, image, label, logits
 
     resnet50_graph = Resnet50Graph()
 
@@ -302,6 +317,7 @@ def main(args):
 
             if args.use_fp16:
                 self.config.enable_amp(True)
+
             self.config.allow_fuse_add_to_output(True)
 
         def build(self):
@@ -315,39 +331,72 @@ def main(args):
 
     resnet50_eval_graph = Resnet50EvalGraph()
 
+    if rank == 0:
+        writer_train_loss = open("graph/train_losses.txt", "w")
+        writer_train_top1_acc = open("graph/train_top1_accuracy.txt", "w")
+        writer_val_top1_acc = open("graph/val_top1_accuracy.txt", "w")
+
     of_losses, of_accuracy = [], []
     for epoch in range(args.num_epochs):
         resnet50_module.train()
 
-        for b in range(len(train_data_loader)):
+        for b in range(vars.batches_per_epoch):
             # oneflow graph train
             start_t = time.time()
 
-            loss, images, labels, logits = resnet50_graph()
+            loss, predictions, images, label, logits = resnet50_graph()
             # dump_to_npy(images, sub=b)
-            # dump_to_npy(labels, sub=b)
+            # dump_to_npy(label, sub=b)
             # dump_to_npy(loss, sub=b)
             # dump_to_npy(logits, sub=b)
+
             end_t = time.time()
+
             if b % args.loss_print_every_n_iter == 0:
+                correct_of = 0.0
+                predictions = predictions.to_consistent(sbp=[flow.sbp.broadcast])
+                predictions = predictions.to_local()
+                of_predictions = predictions.numpy()
+                clsidxs = np.argmax(of_predictions, axis=1)
+                label = label.to_consistent(sbp=[flow.sbp.broadcast])
+                label = label.to_local()
+                label_nd = label.numpy()
+
+                for i in range(vars.total_train_batch_size):
+                    if clsidxs[i] == label_nd[i]:
+                        correct_of += 1
+
+                loss = loss.to_consistent(sbp=[flow.sbp.broadcast])
                 loss = loss.to_local()
-                l = loss.numpy()
-                of_losses.append(l)
-                print(
-                    "rank {} epoch {} train iter {} oneflow loss {}, train time : {}".format(
-                        rank, epoch, b, l, end_t - start_t
+                loss_np = loss.numpy()
+                of_losses.append(loss_np)
+
+                if vars.rank == 0:
+                    # writer_train_loss.write("%f\n" % l)
+                    # writer_train_loss.flush()
+                    # writer_train_top1_acc.write("%f\n" % (correct_of / total_train_batch_size))
+                    # writer_train_top1_acc.flush()
+                    print(
+                        "{}: epoch {}, iter {}, loss: {:.6f}, top_1: {:.6f}, top_k: {:.6f}, samples/s: {:.3f}".format(
+                            "train", epoch, b, loss_np, correct_of / vars.total_train_batch_size, -1, -1
+                        )
                     )
-                )
-            if b >= 100:
-                break
-        break
-        print("rank %d epoch %d train done, start validation" % (rank, epoch))
+
+            # if b >= 100:
+            #     break
+
+        # break
+
+        # begin eval
+        print("rank {} epoch {} train done, start validation".format(vars.rank, epoch))
 
         resnet50_module.eval()
         correct_of = 0.0
         num_val_samples = 0.0
-        for b in range(len(val_data_loader)):
+
+        for b in range(vars.val_batches_per_epoch):
             start_t = time.time()
+
             predictions, label = resnet50_eval_graph()
 
             predictions = predictions.to_consistent(sbp=[flow.sbp.broadcast])
@@ -364,11 +413,19 @@ def main(args):
 
             correct_of += (clsidxs == label_nd).sum()
             num_val_samples += label_nd.size
+
             end_t = time.time()
 
         top1 = correct_of / num_val_samples
         of_accuracy.append(top1)
-        print("rank %d epoch %d, oneflow top1 val acc: %f" % (rank, epoch, top1))
+        if vars.rank == 0:
+            writer_val_top1_acc.write("%f\n" % (correct_of / vars.total_train_batch_size))
+            writer_val_top1_acc.flush()
+            print(
+                "{}: epoch {}, top_1: {:.6f}, top_k: {:.6f}, samples/s: {:.3f}".format(
+                    "validation", epoch, top1, -1, -1
+                )
+            )
 
         flow.save(
             resnet50_module.state_dict(),
@@ -379,18 +436,20 @@ def main(args):
             consistent_dst_rank=0
         )
 
-    if rank == 0:
-        writer = open("graph_of_losses.txt", "w")
-        for o in of_losses:
-            writer.write("%f\n" % o)
-        writer.close()
+    # if vars.rank == 0:
+    #     writer = open("graph_of_losses.txt", "w")
+    #     for o in of_losses:
+    #         writer.write("%f\n" % o)
+    #     writer.close()
 
-        writer = open("graph/accuracy.txt", "w")
-        for o in of_accuracy:
-            writer.write("%f\n" % o)
-        writer.close()
+    #     writer = open("graph/accuracy.txt", "w")
+    #     for o in of_accuracy:
+    #         writer.write("%f\n" % o)
+    #     writer.close()
 
 
 if __name__ == "__main__":
     args = parse_args()
+    assert _GLOBAL_VARS is None
+    _GLOBAL_VARS = GlobalVars(args)
     main(args)
