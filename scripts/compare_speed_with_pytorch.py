80af7c0732aaef1ecf51e607f66521d5a4089ee5
daquexian <daquexian566@gmail.com>
2021-08-25 14:26:09 +0800

    add memory usage and print_if_rank0 (#127)
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- scripts/compare_speed_with_pytorch.py
+++ scripts/compare_speed_with_pytorch.py
@@ -6,6 +6,8 @@ import os
 import importlib.util
 import argparse
 from typing import Sequence
+import subprocess
+import re
 
 
 import oneflow as flow
@@ -13,6 +15,7 @@ import oneflow._oneflow_internal as oneflow_internal
 
 
 DEFAULT_TIMES = 20
+gpu_memory_used_by_pytorch = 0
 
 
 def import_file(path):
@@ -27,6 +30,24 @@ def sync(x):
         x.numpy()
     else:
         x.cpu()
+
+
+def gpu_memory_used():
+    output = subprocess.check_output(['nvidia-smi', '--query-compute-apps=pid,used_gpu_memory', '--format=csv,noheader'])
+    output = output.decode('utf-8').strip()
+    my_pid = os.getpid()
+    mem_used_by_me = 0
+    for line in output.split('\n'):
+        pid, mem_used = map(int, re.split(",? ", line)[:2])
+        if pid == my_pid:
+            mem_used_by_me += mem_used
+    return mem_used_by_me
+
+
+def print_rank_0(*args, **kwargs):
+    rank = int(os.getenv("RANK", "0"))
+    if rank == 0:
+        print(*args, **kwargs)
 
 
 def test(
@@ -119,11 +140,18 @@ def test(
     total_time_ms = (end - start) * 1000
     time_per_run_ms = total_time_ms / times
     if no_verbose:
-        print(f"{framework_name}: {time_per_run_ms:.1f}ms")
+        print_rank_0(f"{framework_name}: {time_per_run_ms:.1f}ms")
     else:
-        print(
+        print_rank_0(
             f"{framework_name} {module_name} time: {time_per_run_ms:.1f}ms (= {total_time_ms:.1f}ms / {times}, input_shape={input_shape}, backward is {'disabled' if disable_backward else 'enabled'})"
         )
+    global gpu_memory_used_by_pytorch
+    if test_oneflow:
+        print_rank_0(f"{framework_name} GPU used (rank 0): {gpu_memory_used() - gpu_memory_used_by_pytorch} MiB")
+    else:
+        gpu_memory_used_by_pytorch = gpu_memory_used()
+
+        print_rank_0(f"{framework_name} GPU used (rank 0): {gpu_memory_used_by_pytorch} MiB")
     if ddp and not test_oneflow:
         import torch.distributed as dist
 
@@ -147,6 +175,7 @@ if __name__ == "__main__":
 
     global test_oneflow
 
+    # NOTE: PyTorch must run before OneFlow for correct memory usage
     test_oneflow = False
     pytorch_time = test(
         args.model_path,
@@ -170,8 +199,8 @@ if __name__ == "__main__":
     )
     relative_speed = pytorch_time / oneflow_time
     if args.no_verbose:
-        print(f"Relative speed: {relative_speed:.2f}")
+        print_rank_0(f"Relative speed: {relative_speed:.2f}")
     else:
-        print(
+        print_rank_0(
             f"Relative speed: {relative_speed:.2f} (= {pytorch_time:.1f}ms / {oneflow_time:.1f}ms)"
         )
