230eab6973ff73d71829d0746781b96bcdd2cbe0
leaves-zwx <kunta0932@gmail.com>
2021-08-27 16:16:38 +0800

    train_ddp and train_eager

--- resnet50/examples/train_eager.sh
+++ resnet50/examples/train_eager.sh
@@ -11,13 +11,7 @@ export NCCL_LAUNCH_MODE=PARALLEL
 echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
 export NCCL_DEBUG=INFO
 
-# OFRECORD_PATH="/DATA/disk1/ImageNet/ofrecord/"
-OFRECORD_PATH=/dataset/ImageNet/ofrecord/ 
-CHECKPOINT_PATH=graph_checkpoints
-
-if [ ! -d "$CHECKPOINT_PATH" ]; then
-    mkdir $CHECKPOINT_PATH
-fi
+OFRECORD_PATH=/dataset/ImageNet/ofrecord/
 
 OFRECORD_PART_NUM=256
 LEARNING_RATE=0.768
@@ -30,7 +24,6 @@ VAL_BATCH_SIZE=50
 SRC_DIR=$(realpath $(dirname $0)/..)
 
 python3 $SRC_DIR/train.py \
-    --save $CHECKPOINT_PATH \
     --ofrecord-path $OFRECORD_PATH \
     --ofrecord-part-num $OFRECORD_PART_NUM \
     --num-devices-per-node $DEVICE_NUM_PER_NODE \
@@ -39,23 +32,3 @@ python3 $SRC_DIR/train.py \
     --num-epochs $EPOCH \
     --train-batch-size $TRAIN_BATCH_SIZE \
     --val-batch-size $VAL_BATCH_SIZE \
-    # --print-interval 10 \
-    # --batches-per-epoch 80 \
-    # --val-batches-per-epoch 10 \
-
-# python3 -m oneflow.distributed.launch \
-#     --nproc_per_node $DEVICE_NUM_PER_NODE \
-#     --nnodes $NUM_NODES \
-#     --node_rank $NODE_RANK \
-#     --master_addr $MASTER_ADDR \
-#     $SRC_DIR/train.py \
-#         --save $CHECKPOINT_PATH \
-#         --ofrecord-path $OFRECORD_PATH \
-#         --ofrecord-part-num $OFRECORD_PART_NUM \
-#         --num-devices-per-node $DEVICE_NUM_PER_NODE \
-#         --lr $LEARNING_RATE \
-#         --momentum $MOM \
-#         --num-epochs $EPOCH \
-#         --train-batch-size $TRAIN_BATCH_SIZE \
-#         --val-batch-size $VAL_BATCH_SIZE \
-#         --ddp \
