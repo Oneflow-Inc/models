951fdf409060200101e4326e704295633c040567
chen_biubiu <73211263+Cristina-cxq@users.noreply.github.com>
2021-08-28 17:39:26 +0800

    Delete train_oneflow.sh

--- FODDet/train_oneflow.sh
+++
@@ -1,7 +0,0 @@
- python train.py \
-      --save_checkpoint_path $CHECKPOINT_PATH \
-      --DATA_DIR $DATASET_PATH \
-      --lr 0.001 \
-      --epoch 50 \
-      --batch_size 8 \
-      # --checkpoint $LOAD_CHECKPOINT
