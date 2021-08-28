7c6f82909607149355c4006ac101d3ccca9fcb23
chen_biubiu <73211263+Cristina-cxq@users.noreply.github.com>
2021-08-28 17:52:19 +0800

    Add files via upload

---
+++ FODDet/test/visualize.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
