01bbf01a34599dbce7e420455daaa45cff2794d1
YananLee <77721596+YanLee97@users.noreply.github.com>
2021-08-30 10:43:22 +0800

    Add CSRNet (#76)
    * Add CSRNet
    
    * Create ttt.py
    
    * Add files via upload
    
    * Delete vgg.py
    
    * Delete val.py
    
    * Delete utils.py
    
    * Delete train.py
    
    * Delete requirement.txt
    
    * Delete part_B_val.json
    
    * Delete part_B_train_with_val.json
    
    * Delete part_B_train.json
    
    * Delete part_B_test.json
    
    * Delete part_A_val.json
    
    * Delete part_A_train_with_val.json
    
    * Delete part_A_train.json
    
    * Delete part_A_test.json
    
    * Delete model.py
    
    * Delete make_dateset.py
    
    * Delete image.py
    
    * Delete dataset.py
    
    * Delete spatial_transforms.py
    
    * Delete temporal_transforms.py
    
    * Delete README.md
    
    * Delete ttt.py
    
    * Create README.md
    
    * Update README.md
    
    * Create val.sh
    
    * Create train.sh
    
    * Update README.md
    
    * Update README.md
    
    * Create test.py
    
    * Create infer.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update temporal_transforms.py
    
    * Update train.py
    
    * Update test.py
    
    * Update model.py
    
    * Update infer.sh
    
    * Rename test.py to infer.py
    
    * Delete make_dateset.py
    
    * Update train.py
    
    * Update train.sh
    
    * Update utils.py
    
    * Update val.py
    
    * Update val.sh
    
    * Update dataset.py

---
+++ CSRNet/val.sh
@@ -0,0 +1,38 @@
+set -aux
+
+PRETRAIN_MODEL_PATH="vgg_imagenet_pretrain_model/"
+MODEL_PATH="checkpoint/"
+Data_PATH="dataset"
+Model="Shanghai_BestModelA" #choose  from  Shanghai_BestModelA,Shanghai_BestModelB
+
+if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
+  mkdir ${PRETRAIN_MODEL_PATH}
+fi
+
+
+if [ ! -d "${PRETRAIN_MODEL_PATH}vgg16_oneflow_model" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/classification/vgg_models/vgg16_oneflow_model.tar.gz
+  tar zxf vgg16_oneflow_model.tar.gz --directory ${PRETRAIN_MODEL_PATH}
+fi
+
+if [ ! -d "$MODEL_PATH" ]; then
+  mkdir ${MODEL_PATH}
+fi
+
+if [ ! -d "${MODEL_PATH}${Model}" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/CSRNet/${Model}.rar
+  unrar x ${Model}  ${MODEL_PATH}
+fi
+
+
+if [ ! -d "${Data_PATH}" ]; then
+
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/datasets/models/CSRNet/Shanghai_dataset.rar
+  unrar x Shanghai_dataset
+  mv Shanghai_dataset ${Data_PATH}
+
+fi
+
+
+python3 val.py 'checkpoint/Shanghai_BestModelB/shanghaiB_bestmodel'  'part_B_test'
+#python3 val.py 'checkpoint/Shanghai_AestModelB/shanghaiA_bestmodel'  'part_A_test'
