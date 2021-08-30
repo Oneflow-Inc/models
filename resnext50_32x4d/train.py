594ef8cccd08d821c843cc3faf4e879ed683573d
rentainhe <596106517@qq.com>
2021-08-30 22:04:17 +0800

    update train and infer

--- resnext50_32x4d/train.py
+++ resnext50_32x4d/train.py
@@ -73,7 +73,7 @@ def main(args):
 
     of_losses = []
     all_samples = len(val_data_loader) * args.val_batch_size
-    print_interval = 100
+    print_interval = 20
 
     for epoch in range(args.epochs):
         resnext50_32x4d_module.train()
