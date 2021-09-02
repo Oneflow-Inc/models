86d76bb4c4a45110d96f3720919b4584904f26d8
Ying <yinger_z@126.com>
2021-09-02 12:41:09 +0800

    added speech-emotion-analyzer

---
+++ Speech-Emotion-Analyzer/models/lstm_ser.py
@@ -0,0 +1,31 @@
+from .lstm_oneflow import LSTM
+import oneflow.nn as nn
+
+
+class lstm_ser(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, batch_size):
+        super(lstm_ser, self).__init__()
+        # self.lstm_feature = LSTM(input_dim, hidden_dim, hidden_dim)
+        self.classifier = nn.Sequential(
+            LSTM(input_dim, hidden_dim, batch_size),
+            nn.Dropout(0.5),
+            nn.Linear(hidden_dim, 32),
+            nn.ReLU(),
+            nn.Linear(32, output_dim),
+        )
+
+    def forward(self, x):
+        # x = self.lstm_feature(x)
+
+        logits = self.classifier(x)
+        return logits
+
+
+if __name__ == '__main__':
+    model = lstm_ser(312, 256, 10, 32)
+    model.to("cuda")
+    arr = np.random.randn(1, 32, 312)
+    input = flow.Tensor(arr, device="cuda")
+    output = model(input)
+
+    print(output.shape)
