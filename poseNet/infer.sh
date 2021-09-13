a23c786ac577bc5914bbf14ec5e4ba09ba55104a
YifanGe <54209775+gemessss@users.noreply.github.com>
2021-09-13 13:59:55 +0800

    Update infer.sh

--- poseNet/infer.sh
+++ poseNet/infer.sh
@@ -1,7 +1,7 @@
 set -aux
 
 PRETRAIN_MODEL_PATH="epoch_959_val_acc_0.906250"
-IMAGE_PATH="data/1-4.jpg"
+IMAGE_PATH="data/0-1.jpg"
 
 if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
   wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/pose/posenet_pretrain_model.tar.gz
