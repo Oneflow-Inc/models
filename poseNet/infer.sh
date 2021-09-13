e9f631266ec3f371c95ada52af5c61f56776d624
YifanGe <54209775+gemessss@users.noreply.github.com>
2021-09-13 18:23:33 +0800

    Update infer.sh

--- poseNet/infer.sh
+++ poseNet/infer.sh
@@ -8,4 +8,9 @@ if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
   tar zxf posenet_pretrain_model.tar.gz
 fi
 
+if [ ! -d "IMAGE_PATH" ]; then
+  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/pose/data.tar.gz
+  tar zxf data.tar.gz
+fi
+
 python3 infer.py --model_path $PRETRAIN_MODEL_PATH --image_path $IMAGE_PATH
