0f6d090157dc21eb1a587d818e352b727c3b9f0a
x54-729 <45304952+x54-729@users.noreply.github.com>
2021-09-22 10:16:31 +0800

    update LSTM (#210)
    * small bug in infer.sh
    
    * using nn.layernorm
    
    * add layers and remove some parameters
    
    * space

--- NLP/LSTMText/train.sh
+++ NLP/LSTMText/train.sh
@@ -10,10 +10,9 @@ EPOCH=30
 LEARNING_RATE=3e-4
 SEQUENCE_LEN=128
 EMBEDDING_DIM=100
-NFC=128
 HIDDEN_SIZE=256
 
-SAVE_PER_EPOCHS=5
+SAVE_EVERY_EPOCHS=1
 LOAD_DIR="."
 SAVE_DIR="pretrain_model"
 IMDB_PATH="./imdb"
@@ -25,9 +24,9 @@ python3 train_bilstm.py \
     --lr $LEARNING_RATE \
     --sequence_length $SEQUENCE_LEN \
     --emb_dim $EMBEDDING_DIM \
-    --nfc $NFC \
     --hidden_size $HIDDEN_SIZE \
     --model_load_dir $LOAD_DIR \
-    --model_save_every_n_epochs $SAVE_PER_EPOCHS \
+    --model_save_every_n_epochs $SAVE_EVERY_EPOCHS \
     --model_save_dir $SAVE_DIR  \
     --imdb_path "$IMDB_PATH"
+    
