8a0d992fb40831b8cce2829edc772fc6ba9e57b9
yingzhao27 <61409446+yingzhao27@users.noreply.github.com>
2021-09-22 15:28:38 +0800

    Update train.sh

--- Audio/Speech-Emotion-Analyzer/train.sh
+++ Audio/Speech-Emotion-Analyzer/train.sh
@@ -3,11 +3,11 @@ set -aux
 
 OPTIONS_PATH='configs/lstm.yaml'
 
-#python preprocess.py --config $OPTIONS_PATH
+python preprocess.py --config $OPTIONS_PATH
 
 echo "Data prepared !"
 echo "Starting training ..."
 
-CUDA_VISIBLE_DEVICES=2 python train.py --config $OPTIONS_PATH
+python train.py --config $OPTIONS_PATH
 
 echo "Train done!"
