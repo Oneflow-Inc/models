53dec038ac85c063306c6909f8e58b7fa951bae1
yingzhao27 <61409446+yingzhao27@users.noreply.github.com>
2021-10-08 15:08:24 +0800

    updata cmd.sh

--- Audio/Speech-Transformer/egs/aishell/cmd.sh
+++ Audio/Speech-Transformer/egs/aishell/cmd.sh
@@ -13,10 +13,3 @@
 export train_cmd="run.pl --mem 2G"
 export cuda_cmd="run.pl --mem 2G --gpu 1"
 export decode_cmd="run.pl --mem 4G"
-
-# NPU setup
-# export train_cmd="queue.pl -q all.q --mem 2G"
-# export cuda_cmd="/home/work_nfs/common/tools/pyqueue_asr.pl --mem 2G --gpu 1"
-# export decode_cmd="/home/work_nfs/common/tools/pyqueue_asr.pl --mem 4G --gpu 1"
-#export cuda_cmd="queue.pl --mem 2G --gpu 1 --config conf/gpu.conf"
-#export decode_cmd="queue.pl -q all.q --mem 4G"
