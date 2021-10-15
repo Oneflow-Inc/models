46a0615998b5eaab7e4cdadba14a8e5fc2a22606
yingzhao27 <61409446+yingzhao27@users.noreply.github.com>
2021-10-15 15:41:33 +0800

    Update utils.py

--- Audio/Speech-Transformer/src/utils/utils.py
+++ Audio/Speech-Transformer/src/utils/utils.py
@@ -11,7 +11,7 @@ def pad_list(xs, pad_value):
     # From: espnet/src/nets/e2e_asr_th.py: pad_list()
     n_batch = len(xs)
     max_len = max(x.size(0) for x in xs)
-    pad = xs[0].new_ones((n_batch, max_len, xs[0].size()[1:])).fill_(pad_value)
+    pad = xs[0].new_ones((n_batch, max_len, *xs[0].size()[1:])).fill_(pad_value)
 
     for i in range(n_batch):
         pad[i, : xs[i].size(0)] = xs[i]
