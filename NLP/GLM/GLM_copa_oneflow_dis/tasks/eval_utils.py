935ac73f9e7712cc21d46006a435f89793f18247
lcy <2695928308@qq.com>
2021-10-22 17:15:33 +0800

    record time

--- NLP/GLM/GLM_copa_oneflow_dis/tasks/eval_utils.py
+++ NLP/GLM/GLM_copa_oneflow_dis/tasks/eval_utils.py
@@ -96,7 +96,6 @@ def accuracy_func_provider(single_dataset_provider, metric_dict, args, is_test=F
             
             elapsed_time = time.time() - start_time
             print(elapsed_time)
-            exit(0)
             # if output_predictions and torch.distributed.get_rank() == 0:
             if output_predictions:
                 filename = os.path.join(args.log_dir, name + '.jsonl')
