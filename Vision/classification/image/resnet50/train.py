526f86d52b69a35b12a1797fde0aa9f824ab2ee1
ouyangyu <xuanjiuye@gmail.com>
2021-10-26 18:46:30 +0800

    refine

--- Vision/classification/image/resnet50/train.py
+++ Vision/classification/image/resnet50/train.py
@@ -84,7 +84,7 @@ class Trainer(object):
         start_t = time.perf_counter()
 
         if self.is_consistent:
-            placement = flow.env.all_device_placement("cuda")
+            placement = flow.env.all_device_placement("cpu")
             self.model = self.model.to_consistent(
                 placement=placement, sbp=flow.sbp.broadcast
             )
