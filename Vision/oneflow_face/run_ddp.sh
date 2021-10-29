3bd9178ab87e37b386bc3297e9acd59eb0edd58e
olojuwin <502401452@qq.com>
2021-10-29 22:49:22 +0800

    add DEVICE_NUM_PER_NODE

--- Vision/oneflow_face/run_ddp.sh
+++ Vision/oneflow_face/run_ddp.sh
@@ -2,7 +2,7 @@
 
 MASTER_ADDR=127.0.0.1
 MASTER_PORT=17788
-TOTAL_DEVICE_NUM=8
+DEVICE_NUM_PER_NODE=8
 NUM_NODES=1
 NODE_RANK=0
 
@@ -17,9 +17,9 @@ export ONEFLOW_DEBUG_MODE=True
 
 NCCL_DEBUG=INFO \
 python3 -m oneflow.distributed.launch \
---nproc_per_node $TOTAL_DEVICE_NUM \
+--nproc_per_node $DEVICE_NUM_PER_NODE \
 --nnodes $NUM_NODES \
 --node_rank $NODE_RANK \
 --master_addr $MASTER_ADDR \
 --master_port $MASTER_PORT \
-train.py configs/ms1mv3_r50.py \
+train.py configs/ms1mv3_r50.py 
