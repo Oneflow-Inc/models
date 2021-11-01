47a57a48be4cf5ce420ccbae94d9a79bd1170f25
CPFLAME <410070869@qq.com>
2021-11-01 20:28:05 +0800

    modify debug.py

--- NLP/GLM/glm_debug_torch.py
+++ NLP/GLM/glm_debug_torch.py
@@ -276,38 +276,63 @@ class ParallelSelfAttention(flow.nn.Module):
 
         return output
 
+if __name__ == "__main__":
+    hidden_size = 512
+    num_attention_heads = 8
+    attention_dropout_prob = 0.1
+    output_dropout_prob = 0.1
+    init_method = unscaled_init_method(0.2)
+    output_layer_init_method = scaled_init_method(0.2, 12)
+    relative_encoding = False
+    performer = False
+    attention_scale = 1.0
 
-hidden_size = 512
-num_attention_heads = 8
-attention_dropout_prob = 0.1
-output_dropout_prob = 0.1
-init_method = unscaled_init_method(0.2)
-output_layer_init_method = scaled_init_method(0.2, 12)
-relative_encoding = False
-performer = False
-attention_scale = 1.0
+    class AttenModel(flow.nn.Module):
+        def __init__(self, ):
+            super(AttenModel, self).__init__()
+            self.atten = flow.nn.ModuleList(
+                            [ParallelSelfAttention(
+                                hidden_size,
+                                num_attention_heads,
+                                attention_dropout_prob,
+                                output_dropout_prob,
+                                init_method,
+                                output_layer_init_method=output_layer_init_method,
+                                relative_encoding=relative_encoding,
+                                performer=performer,
+                                attention_scale=attention_scale) 
+                                for _ in range(12)
+                            ]
+                        )
 
-attention = ParallelSelfAttention(
-            hidden_size,
-            num_attention_heads,
-            attention_dropout_prob,
-            output_dropout_prob,
-            init_method,
-            output_layer_init_method=output_layer_init_method,
-            relative_encoding=relative_encoding,
-            performer=performer,
-            attention_scale=attention_scale)
+        def forward(self, x, ltor_mask, position_embeddings, r_w_bias, r_r_bias, mem):
+            for layer in self.atten:
+                x = layer(x, ltor_mask, position_embeddings, r_w_bias, r_r_bias, mem)
+            return x
 
-layernorm_output = flow.randn(4, 332, 512)
-ltor_mask = flow.randn(4, 1, 332, 332)
-position_embeddings = None
-r_w_bias = None
-r_r_bias = None
-mem = None
-attention = attention.to("cuda")
-layernorm_output = layernorm_output.to("cuda")
-ltor_mask = ltor_mask.to("cuda")
-
-
-for i in range(10000):
-    attention_output = attention(layernorm_output, ltor_mask, position_embeddings, r_w_bias, r_r_bias, mem)
+    layernorm_output = flow.randn(4, 332, 512)
+    ltor_mask = flow.randn(4, 1, 332, 332)
+    position_embeddings = None
+    r_w_bias = None
+    r_r_bias = None
+    mem = None
+    layernorm_output = layernorm_output.to("cuda")
+    ltor_mask = ltor_mask.to("cuda")
+    # ============= comment code =================
+    model = AttenModel()
+    model.train()
+    model = model.to("cuda")
+    optimizer = flow.optim.Adam(
+                        model.parameters(),
+                        lr=0.01,
+                        )
+    # ============= comment end =================
+    for i in range(10000000):
+        print("training")
+        # ============= comment code =================
+        attention_output = model(layernorm_output, ltor_mask, position_embeddings, r_w_bias, r_r_bias, mem)
+        optimizer.zero_grad()
+        loss = attention_output.sum()
+        loss.backward()
+        optimizer.step()
+        # ============= comment end =================
