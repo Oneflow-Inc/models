29f2c916f9d7f4947305364a8995818443b63b0a
clackhan <han_binbin@163.com>
2021-11-22 10:33:20 +0800

    update agrs

--- RecommenderSystems/wide_and_deep/train_consistent_eager.sh
+++ RecommenderSystems/wide_and_deep/train_consistent_eager.sh
@@ -4,7 +4,7 @@ NUM_NODES=1
 NODE_RANK=0
 DATA_DIR=/dataset/wdl_ofrecord/ofrecord
 EMBD_SIZE=2322444
-BATHSIZE=1024
+BATHSIZE=2048
 
 python3 -m oneflow.distributed.launch \
     --nproc_per_node $DEVICE_NUM_PER_NODE \
@@ -15,10 +15,10 @@ python3 -m oneflow.distributed.launch \
     --learning_rate 0.001 \
     --batch_size $BATHSIZE \
     --data_dir $DATA_DIR \
-    --loss_print_every_n_iter 1000 \
+    --loss_print_every_n_iter 100 \
     --eval_interval 0 \
     --deep_dropout_rate 0.5 \
-    --max_iter 3100 \
+    --max_iter 310 \
     --hidden_units_num 7 \
     --hidden_size 1024 \
     --wide_vocab_size $EMBD_SIZE \
