13843860c3a33db77f4edb9c9ef01fa66db2444c
kaijieshi7 <1638961687@qq.com>
2021-12-01 10:48:25 +0800

    update util

--- Vision/gan/UNIT/util.py
+++ Vision/gan/UNIT/util.py
@@ -3,6 +3,72 @@ from oneflow.optim import lr_scheduler
 import math
 import oneflow.nn.init as init
 import os
+import flowvision.transforms as transforms
+import flowvision.datasets as datasets
+from data import ImageFilelist
+import flowvision.utils as vutils
+import yaml
+import time
+
+
+def get_all_data_loaders(conf):
+    batch_size = conf['batch_size']
+    num_workers = conf['num_workers']
+    if 'new_size' in conf:
+        new_size_a = new_size_b = conf['new_size']
+    else:
+        new_size_a = conf['new_size_a']
+        new_size_b = conf['new_size_b']
+    height = conf['crop_image_height']
+    width = conf['crop_image_width']
+
+    if 'data_root' in conf:
+        train_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'trainA'), batch_size, True,
+                                              new_size_a, height, width, num_workers, True)
+        test_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'testA'), batch_size, False,
+                                             new_size_a, new_size_a, new_size_a, num_workers, True)
+        train_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'trainB'), batch_size, True,
+                                              new_size_b, height, width, num_workers, True)
+        test_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'testB'), batch_size, False,
+                                             new_size_b, new_size_b, new_size_b, num_workers, True)
+    else:
+        train_loader_a = get_data_loader_list(conf['data_folder_train_a'], conf['data_list_train_a'], batch_size, True,
+                                                new_size_a, height, width, num_workers, True)
+        test_loader_a = get_data_loader_list(conf['data_folder_test_a'], conf['data_list_test_a'], batch_size, False,
+                                                new_size_a, new_size_a, new_size_a, num_workers, True)
+        train_loader_b = get_data_loader_list(conf['data_folder_train_b'], conf['data_list_train_b'], batch_size, True,
+                                                new_size_b, height, width, num_workers, True)
+        test_loader_b = get_data_loader_list(conf['data_folder_test_b'], conf['data_list_test_b'], batch_size, False,
+                                                new_size_b, new_size_b, new_size_b, num_workers, True)
+    return train_loader_a, train_loader_b, test_loader_a, test_loader_b
+
+
+def get_data_loader_list(root, file_list, batch_size, train, new_size=None,
+                           height=256, width=256, num_workers=4, crop=True):
+    transform_list = [transforms.ToTensor(),
+                      transforms.Normalize((0.5, 0.5, 0.5),
+                                           (0.5, 0.5, 0.5))]
+    transform_list = [transforms.RandomCrop((height, width))] + transform_list if crop else transform_list
+    transform_list = [transforms.Resize(new_size)] + transform_list if new_size is not None else transform_list
+    transform_list = [transforms.RandomHorizontalFlip()] + transform_list if train else transform_list
+    transform = transforms.Compose(transform_list)
+    dataset = ImageFilelist(root, file_list, transform=transform)
+    loader = flow.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=train, drop_last=True, num_workers=num_workers)
+    return loader
+
+
+def get_data_loader_folder(input_folder, batch_size, train, new_size=None,
+                           height=256, width=256, num_workers=4, crop=True):
+    transform_list = [transforms.ToTensor(),
+                      transforms.Normalize((0.5, 0.5, 0.5),
+                                           (0.5, 0.5, 0.5))]
+    transform_list = [transforms.RandomCrop((height, width))] + transform_list if crop else transform_list
+    transform_list = [transforms.Resize(new_size)] + transform_list if new_size is not None else transform_list
+    transform_list = [transforms.RandomHorizontalFlip()] + transform_list if train else transform_list
+    transform = transforms.Compose(transform_list)
+    dataset = datasets.ImageFolder(input_folder, transform=transform)
+    loader = flow.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=train, drop_last=True, num_workers=num_workers)
+    return loader
 
 def get_scheduler(optimizer, hyperparameters, iterations=-1):
     if 'lr_policy' not in hyperparameters or hyperparameters['lr_policy'] == 'constant':
@@ -13,6 +79,67 @@ def get_scheduler(optimizer, hyperparameters, iterations=-1):
     else:
         return NotImplementedError('learning rate policy [%s] is not implemented', hyperparameters['lr_policy'])
     return scheduler
+
+
+def prepare_sub_folder(output_directory):
+    image_directory = os.path.join(output_directory, 'images')
+    if not os.path.exists(image_directory):
+        print("Creating directory: {}".format(image_directory))
+        os.makedirs(image_directory)
+    checkpoint_directory = os.path.join(output_directory, 'checkpoints')
+    if not os.path.exists(checkpoint_directory):
+        print("Creating directory: {}".format(checkpoint_directory))
+        os.makedirs(checkpoint_directory)
+    return checkpoint_directory, image_directory
+
+
+def __write_images(image_outputs, display_image_num, file_name):
+    image_outputs = [images.expand(-1, 3, -1, -1) for images in image_outputs] # expand gray-scale images to 3 channels
+    image_tensor = flow.cat([images[:display_image_num] for images in image_outputs], 0)
+    image_grid = vutils.make_grid(image_tensor.data, nrow=display_image_num, padding=0, normalize=True)
+    vutils.save_image(image_grid, file_name, nrow=1)
+
+
+def write_2images(image_outputs, display_image_num, image_directory, postfix):
+    n = len(image_outputs)
+    __write_images(image_outputs[0:n//2], display_image_num, '%s/gen_a2b_%s.jpg' % (image_directory, postfix))
+    __write_images(image_outputs[n//2:n], display_image_num, '%s/gen_b2a_%s.jpg' % (image_directory, postfix))
+
+
+def write_one_row_html(html_file, iterations, img_filename, all_size):
+    html_file.write("<h3>iteration [%d] (%s)</h3>" % (iterations,img_filename.split('/')[-1]))
+    html_file.write("""
+        <p><a href="%s">
+          <img src="%s" style="width:%dpx">
+        </a><br>
+        <p>
+        """ % (img_filename, img_filename, all_size))
+    return
+
+
+def write_html(filename, iterations, image_save_iterations, image_directory, all_size=1536):
+    html_file = open(filename, "w")
+    html_file.write('''
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Experiment name = %s</title>
+      <meta http-equiv="refresh" content="30">
+    </head>
+    <body>
+    ''' % os.path.basename(filename))
+    html_file.write("<h3>current</h3>")
+    write_one_row_html(html_file, iterations, '%s/gen_a2b_train_current.jpg' % (image_directory), all_size)
+    write_one_row_html(html_file, iterations, '%s/gen_b2a_train_current.jpg' % (image_directory), all_size)
+    for j in range(iterations, image_save_iterations-1, -1):
+        if j % image_save_iterations == 0:
+            write_one_row_html(html_file, j, '%s/gen_a2b_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_b2a_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_a2b_train_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(html_file, j, '%s/gen_b2a_train_%08d.jpg' % (image_directory, j), all_size)
+    html_file.write("</body></html>")
+    html_file.close()
+
 
 def weights_init(init_type='gaussian'):
     def init_fun(m):
@@ -75,3 +202,24 @@ def get_model_list(dirname, key):
     gen_models.sort()
     last_model_name = gen_models[-1]
     return last_model_name
+
+def get_config(config):
+    with open(config, 'r') as stream:
+        return yaml.load(stream)
+
+class Timer:
+    def __init__(self, msg):
+        self.msg = msg
+        self.start_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        print(self.msg % (time.time() - self.start_time))
+
+def write_loss(iterations, trainer, train_writer):
+    members = [attr for attr in dir(trainer) \
+               if not callable(getattr(trainer, attr)) and not attr.startswith("__") and ('loss' in attr or 'grad' in attr or 'nwd' in attr)]
+    for m in members:
+        train_writer.add_scalar(m, getattr(trainer, m), iterations + 1)
