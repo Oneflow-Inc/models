e8099c9e70c056701e0c9a7f4a392d420161ce90
clackhan <han_binbin@163.com>
2021-12-02 16:54:09 +0800

    update

--- RecommenderSystems/wide_and_deep/train_consistent_graph.sh
+++ RecommenderSystems/wide_and_deep/train_consistent_graph.sh
@@ -4,14 +4,20 @@ NUM_NODES=1
 NODE_RANK=0
 DATA_DIR=/dataset/wdl_ofrecord/ofrecord
 EMBD_SIZE=2322444
-BATHSIZE=2048
+BATHSIZE=1024
+
+export ONEFLOW_KERNEL_ENABLE_CUDA_GRAPH=1
+export ONEFLOW_THREAD_ENABLE_LOCAL_MESSAGE_QUEUE=1
+export ONEFLOW_KERNEL_DISABLE_BLOB_ACCESS_CHECKER=1
+export ONEFLOW_ACTOR_ENABLE_LIGHT_ACTOR=1
+export ONEFLOW_STREAM_REUSE_CUDA_EVENT=1
 
 python3 -m oneflow.distributed.launch \
     --nproc_per_node $DEVICE_NUM_PER_NODE \
     --nnodes $NUM_NODES \
     --node_rank $NODE_RANK \
     --master_addr $MASTER_ADDR \
-    train.py \
+  train.py \
     --learning_rate 0.001 \
     --batch_size $BATHSIZE \
     --data_dir $DATA_DIR \
@@ -19,7 +25,7 @@ python3 -m oneflow.distributed.launch \
     --eval_interval 0 \
     --deep_dropout_rate 0.5 \
     --max_iter 310 \
-    --hidden_units_num 7 \
+    --hidden_units_num 2 \
     --hidden_size 1024 \
     --wide_vocab_size $EMBD_SIZE \
     --deep_vocab_size $EMBD_SIZE \
@@ -27,3 +33,4 @@ python3 -m oneflow.distributed.launch \
     --data_part_name_suffix_length 5 \
     --execution_mode 'graph' \
     --test_name 'train_eager_graph_'$DEVICE_NUM_PER_NODE'gpu'
+    # --use_synthetic_data
