e7cf15ba77c334c93cd0389c945f318dec9c1da1
ShawnXuan <xiexuanx2@gmail.com>
2022-01-06 11:52:39 +0800

    update

--- RecommenderSystems/dlrm/config.py
+++ RecommenderSystems/dlrm/config.py
@@ -47,7 +47,7 @@ def get_args(print_args=True):
         help="do eval after_training",
     )
     parser.add_argument(
-        "--dataset_format", type=str, default="ofrecord", help="ofrecord, onerec or synthetic"
+        "--dataset_format", type=str, default="ofrecord", help="ofrecord, onerec, parquet or synthetic"
     )
     parser.add_argument("--data_part_num", type=int, default=256)
     parser.add_argument(
