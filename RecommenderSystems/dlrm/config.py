a621dcee4e8562898930fb7e352da83779bda7fe
XIE Xuan <xiexuanx2@gmail.com>
2022-01-18 14:50:43 +0800

    Dev dlrm eval partnum (#284)
    * eval data part number
    
    * fix

--- RecommenderSystems/dlrm/config.py
+++ RecommenderSystems/dlrm/config.py
@@ -50,6 +50,7 @@ def get_args(print_args=True):
         "--dataset_format", type=str, default="ofrecord", help="ofrecord, onerec, parquet or synthetic"
     )
     parser.add_argument("--data_part_num", type=int, default=256)
+    parser.add_argument("--eval_data_part_num", type=int, default=256)
     parser.add_argument(
         "--data_dir", type=str, default="/dataset/wdl_ofrecord/ofrecord"
     )
