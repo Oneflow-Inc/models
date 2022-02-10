f0fba591c9cbde3c7d8e4924cee0541d80ecafed
mls1999725 <546278509@qq.com>
2022-02-10 14:35:50 +0800

    yolov5-profile-complement

--- ops/nms.py
+++ ops/nms.py
@@ -1,5 +1,4 @@
 import oneflow as flow
-import oneflow as flow_exp
 from oneflow import Tensor
 
 
@@ -7,7 +6,7 @@ def nms(boxes: Tensor, scores: Tensor, iou_threshold: float) -> Tensor:
     scores_inds = flow_exp.argsort(scores, dim=0, descending=True)
     boxes = flow._C.gather(boxes, scores_inds, axis=0)
     _nms_op = (
-        flow_exp.builtin_op("nms")
+        flow.builtin_op("nms")
         .Input("in")
         .Output("out")
         .Attr("iou_threshold", iou_threshold)
