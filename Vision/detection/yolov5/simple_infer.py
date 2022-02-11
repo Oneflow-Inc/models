923283ab0aac986ea59bc286173a5de997e7f5b1
mls1999725 <546278509@qq.com>
2022-02-11 15:39:01 +0800

    add torch profile

--- Vision/detection/yolov5/simple_infer.py
+++ Vision/detection/yolov5/simple_infer.py
@@ -20,18 +20,24 @@ from utils.flow_utils import time_sync
 model = DetectMultiBackend(weights=ROOT / 'yolov5_ckpt', device='cuda:0', dnn=False, data=ROOT / 'data/coco128.yaml')
 model = model.eval()
 
-# t1 = time_sync()
 np.random.seed(0)
-im = flow.tensor(np.random.rand(1, 3, 640, 640)).to('cuda:0')
+im = flow.tensor(np.random.rand(4, 3, 640, 640)).to('cuda:0')
 im = im.float()
-flow._oneflow_internal.profiler.RangePush('yolov5')
-for i in range(20):
-    flow._oneflow_internal.profiler.RangePush('iter{}'.format(i))
+# 预热
+for i in range(5):
     pred = model(im, augment=False, visualize=False)
-    # out = pred.cpu().numpy()
+    out = pred.numpy()
+t1 = time_sync()
+flow._oneflow_internal.profiler.RangePush("oneflow-yolov5-infer")
+for i in range(20):
+    flow._oneflow_internal.profiler.RangePush("forward")
+    pred = model(im, augment=False, visualize=False)
+    flow._oneflow_internal.profiler.RangePop()
+    flow._oneflow_internal.profiler.RangePush('.numpy')
+    out = pred.numpy()
     flow._oneflow_internal.profiler.RangePop()
 flow._oneflow_internal.profiler.RangePop()
-# t2 = time_sync()
+t2 = time_sync()
 # flow.save(model.state_dict(), "./yolov5_model")
 # np.save(ROOT / "pred_oneflow.npy", out)
-# print("time of inference: ", t2 - t1)
+print("time of inference: ", t2 - t1)
---
+++ Vision/detection/yolov5/yolov5_torch/simple_infer.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import cv2
+import torch
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.torch_utils import time_sync
+
+model = DetectMultiBackend(weights=ROOT / 'yolov5s.pt', device='cuda:0', dnn=False, data=ROOT / 'data/coco128.yaml')
+model = model.eval()
+
+# t1 = time_sync()
+np.random.seed(0)
+im = torch.tensor(np.random.rand(4, 3, 640, 640)).to('cuda:0')
+im = im.float()
+# 预热
+for i in range(5):
+    pred = model(im, augment=False, visualize=False)
+    out = pred.cpu().numpy()
+t1 = time_sync()
+torch.cuda.nvtx.range_push("torch-yolov5-infer")
+for i in range(20):
+    torch.cuda.nvtx.range_push("forward")
+    pred = model(im, augment=False, visualize=False)
+    torch.cuda.nvtx.range_pop()
+    torch.cuda.nvtx.range_push(".numpy")
+    out = pred.cpu().numpy()
+    torch.cuda.nvtx.range_pop()
+torch.cuda.nvtx.range_pop()
+t2 = time_sync()
+# torch.save(model.state_dict(), "./yolov5.pt")
+# np.save(ROOT / "pred_torch.npy", out)
+print("time of inference: ", t2 - t1)
