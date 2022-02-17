bdabbccb9c5cdea0a1179352ce505b0e382d836b
kaijieshi7 <1638961687@qq.com>
2022-02-17 10:23:03 +0000

    a

--- Vision/classification/image/mae/pretrain.py
+++ Vision/classification/image/mae/pretrain.py
@@ -170,12 +170,12 @@ def main(args):
     for epoch in range(args.start_epoch, args.epochs):
         # if args.distributed:
         data_loader_train.sampler.set_epoch(epoch)
-        # train_stats = train_one_epoch(
-        #     model, data_loader_train,
-        #     optimizer, device, epoch, loss_scaler,
-        #     log_writer=log_writer,
-        #     args=args
-        # )
+        train_stats = train_one_epoch(
+            model, data_loader_train,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args
+        )
         if args.output_dir and (epoch % 20 == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
