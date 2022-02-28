dcf8dd97c602ec3c6dd44cf48debb9d613bc4b06
ShawnXuan <xiexuanx2@gmail.com>
2022-02-28 18:46:09 +0800

    update

--- RecommenderSystems/dlrm/graph_train.py
+++ RecommenderSystems/dlrm/graph_train.py
@@ -197,7 +197,7 @@ class Trainer(object):
             strtime = time.strftime("%Y-%m-%d %H:%M:%S")
             print(f'Rank[{self.rank}], Iter {self.cur_iter}, AUC {auc:0.5f}, ' +
                   f'#Samples {labels.shape[0]}, Host_Memory {host_mem_mb} MiB, ' +
-                  f'Device_Memory {device_mem_str}, AUC_time {auc_time} s, {strtime}')
+                  f'Device_Memory {device_mem_str}, AUC_time {auc_time:0.1f} s, {strtime}')
             if self.save_model_after_each_eval:
                 self.save_model(f"iter_{self.cur_iter}_val_auc_{auc}")
 
