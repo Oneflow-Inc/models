f7c29375dd03560e9303f454f6700dd53b776e1d
guo-ran <360112263@qq.com>
2022-03-03 17:21:11 +0800

    format

--- RecommenderSystems/dlrm/lr_scheduler.py
+++ RecommenderSystems/dlrm/lr_scheduler.py
@@ -5,21 +5,19 @@ import oneflow as flow
 
 def make_lr_scheduler(args, optimizer):
     warmup_lr = flow.optim.lr_scheduler.LinearLR(
-    optimizer,
-    start_factor=0,
-    total_iters=args.warmup_batches,
+        optimizer, start_factor=0, total_iters=args.warmup_batches,
     )
     poly_decay_lr = flow.optim.lr_scheduler.PolynomialLR(
-    optimizer,
-    steps=args.decay_batches,
-    end_learning_rate=0,
-    power=2.0,
-    cycle=False,
+        optimizer,
+        steps=args.decay_batches,
+        end_learning_rate=0,
+        power=2.0,
+        cycle=False,
     )
     sequential_lr = flow.optim.lr_scheduler.SequentialLR(
-    optimizer=optimizer,
-    schedulers=[warmup_lr, poly_decay_lr],
-    milestones=[args.decay_start],
-    interval_rescaling=True,
+        optimizer=optimizer,
+        schedulers=[warmup_lr, poly_decay_lr],
+        milestones=[args.decay_start],
+        interval_rescaling=True,
     )
     return sequential_lr
