ef79e265e5e4dd0f73bbf2686340abec8fe67073
ShawnXuan <xiexuanx2@gmail.com>
2022-03-21 23:14:07 +0800

    update

--- RecommenderSystems/dlrm/config.py
+++ RecommenderSystems/dlrm/config.py
@@ -33,7 +33,7 @@ def get_args(print_args=True):
     parser.add_argument(
         "--disable_interaction_padding",
         action="store_true",
-        help="disenable interaction padding or not",
+        help="disable interaction padding or not",
     )
     parser.add_argument(
         "--interaction_itself", action="store_true", help="interaction itself or not"
@@ -65,7 +65,7 @@ def get_args(print_args=True):
     )
     parser.add_argument("--store_type", type=str, default="device_host")
     parser.add_argument("--cache_memory_budget_mb_per_rank", type=int, default=8192)
-    parser.add_argument("--use_fp16", action="store_true", help="Run model with amp")
+    parser.add_argument("--amp", action="store_true", help="Run model with amp")
     parser.add_argument("--loss_scale_policy", type=str, default="static", help="static or dynamic")
 
     args = parser.parse_args()
