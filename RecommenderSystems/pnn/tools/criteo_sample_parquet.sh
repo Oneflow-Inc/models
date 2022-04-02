c11483cb5c5c4edd6355a49cea803bf5a0c2b396
BakerMara <550165764@qq.com>
2022-04-02 13:52:13 +0800

    noready

---
+++ RecommenderSystems/pnn/tools/criteo_sample_parquet.sh
@@ -0,0 +1,5 @@
+python criteo_parquet.py \
+    --input_dir=/minio/sdb/liuxinman/criteo_sample \
+    --output_dir=/minio/sdb/liuxinman/criteo_sample/pnn_parquet \
+    --spark_tmp_dir=/minio/sdb/liuxinman/criteo_sample/spark_tmp_dir \
+    --export_dataset_info
