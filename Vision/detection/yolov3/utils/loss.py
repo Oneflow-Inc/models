85b18a9247b313fab666cb620e48f58d1a90ca9e
mls1999725 <546278509@qq.com>
2022-04-14 09:52:22 +0000

    update loss

--- Vision/detection/yolov3/utils/loss.py
+++ Vision/detection/yolov3/utils/loss.py
@@ -89,8 +89,11 @@ class ComputeLoss:
         h = model.hyp  # hyperparameters
 
         # Define criteria
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=flow.tensor([h['cls_pw']], device=device))
-        BCEobj = nn.BCEWithLogitsLoss(pos_weight=flow.tensor([h['obj_pw']], device=device))
+        # BCEcls = nn.BCEWithLogitsLoss(pos_weight=flow.tensor([h['cls_pw']], device=device))
+        # BCEobj = nn.BCEWithLogitsLoss(pos_weight=flow.tensor([h['obj_pw']], device=device))
+
+        BCEcls = BCEBlurWithLogitsLoss()
+        BCEobj = BCEBlurWithLogitsLoss()
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
         self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
