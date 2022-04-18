e9eb3c985f44256db0c8ea648886a145c0ddfde4
ShawnXuan <xiexuanx2@gmail.com>
2022-04-18 15:26:42 +0800

    fix sparse missing value

--- RecommenderSystems/dlrm/tools/criteo1t_parquet.scala
+++ RecommenderSystems/dlrm/tools/criteo1t_parquet.scala
@@ -20,7 +20,7 @@ val val_csv = s"${tmp_dir}/val.csv"
 
 val make_label = udf((str:String) => str.toFloat)
 val make_dense = udf((str:String) => if (str == null) 1  else str.toFloat + 1)
-val make_sparse = udf((str:String, i:Long) => (if (str == null) 0L else Math.floorMod(Integer.parseUnsignedInt(str, 16).toLong, 40000000L)) +  i * 40000000L)
+val make_sparse = udf((str:String, i:Long) => (if (str == null) i * 40000000L else Math.floorMod(Integer.parseUnsignedInt(str, 16).toLong, 40000000L)) +  i * 40000000L)
 val label_cols = Seq(make_label($"label").as("label"))
 val dense_cols = 1.to(13).map{i=>make_dense(col(s"I$i")).as(s"I${i}")}
 val sparse_cols = 1.to(26).map{i=>make_sparse(col(s"C$i"), lit(i)).as(s"C${i}")}
