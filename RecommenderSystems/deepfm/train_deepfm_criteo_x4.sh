0a254de43ae5a67b847f7d3686c08c5645038c4d
Liuxinman <xinman2@ualberta.ca>
2022-04-24 18:52:01 +0800

    Refine train/val/test

--- RecommenderSystems/deepfm/train_deepfm_criteo_x4.sh
+++ RecommenderSystems/deepfm/train_deepfm_criteo_x4.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm -r /minio/sdb/liuxinman/persistent1/*
-rm -r /minio/sdb/liuxinman/persistent2/*
+# rm -r /minio/sdb/liuxinman/persistent1/*
+# rm -r /minio/sdb/liuxinman/persistent2/*
 
-export CUDA_VISIBLE_DEVICES=1,3
+export CUDA_VISIBLE_DEVICES=2
 
 DEVICE_NUM_PER_NODE=1
 NUM_NODES=1
 NODE_RANK=0
 MASTER_ADDR=127.0.0.1
-DATA_DIR=/minio/sdb/liuxinman/criteo_x4_9ea3bdfc/deepfm_parquet
+DATA_DIR=/minio/sdb/liuxinman/dataset/criteo_x4_9ea3bdfc/deepfm_parquet
 PERSISTENT_PATH=/minio/sdb/liuxinman/persistent1
 PERSISTENT_PATH_FM=/minio/sdb/liuxinman/persistent2
 
@@ -21,18 +21,20 @@ python3 -m oneflow.distributed.launch \
       --data_dir $DATA_DIR \
       --persistent_path $PERSISTENT_PATH \
       --persistent_path_fm $PERSISTENT_PATH_FM \
-      --table_size_array "36, 91, 112, 29, 211, 94, 70, 46, 83, 5, 24, 30, 42, 799, 544, 22916, 25358, 200, 13, 10126, 383, 3, 16872, 4508, 23375, 3106, 27, 7077, 24371, 10, 3307, 1628, 4, 23796, 13, 15, 15846, 61, 12817" \
+      --table_size_array "43, 98, 121, 41, 219, 112, 79, 68, 91, 5, 26, 36, 70, 1447, 554, 157461, 117683, 305, 17, 11878, 629, 4, 39504, 5128, 156729, 3175, 27, 11070, 149083, 11, 4542, 1996, 4, 154737, 17, 16, 52989, 81, 40882" \
       --store_type 'cached_host_mem' \
       --cache_memory_budget_mb 1024 \
-      --train_batch_size 10000 \
-      --train_batches 35000 \
+      --batch_size 10000 \
+      --train_batches 50000 \
       --loss_print_interval 100 \
-      --eval_batch_size 10000 \
-      --eval_batches 1000 \
       --dnn "1000,1000,1000,1000,1000" \
       --net_dropout 0.2 \
       --learning_rate 0.001 \
       --embedding_vec_size 16 \
       --disable_fusedmlp True \
       --num_train_samples 36672493 \
+      --num_val_samples 4584062 \
+      --num_test_samples 4584062 \
+      --model_save_dir /minio/sdb/liuxinman/saved_models/of_deepfm \
+      --model_load_dir /minio/sdb/liuxinman/saved_models/of_deepfm/fuxi_to_of \
       > run.log & tail -f run.log
