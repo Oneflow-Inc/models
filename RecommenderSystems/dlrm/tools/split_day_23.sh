8d42e41aae64720a1fe99734ed81c63089f972a5
XIE Xuan <xiexuanx2@gmail.com>
2022-04-27 17:18:49 +0800

    Update scala tool (#338)
    * update dlrm tool default path
    
    * makeDlrmDataset function
    
    * mod_idx->modIdx
    
    * update
    
    * fix
    
    * step by step

--- RecommenderSystems/dlrm/tools/split_day_23.sh
+++ RecommenderSystems/dlrm/tools/split_day_23.sh
@@ -1,8 +1,8 @@
 # split day_23 to test.csv and val.csv
-src_dir="/workspace/dataset/criteo1t/raw"
-tmp_dir="/workspace/tmp_spark"
+src_dir=${1:-"/workspace/dataset/criteo1t/raw"}
+tmp_dir=${2:-"/workspace/tmp_spark"}
 
-mkdir -p /workspace/tmp_spark
+mkdir -p $tmp_dir
 
 # total 178274637, test 89137319, val 89137318
 head -n 89137319 $src_dir/day_23 > $tmp_dir/test.csv
