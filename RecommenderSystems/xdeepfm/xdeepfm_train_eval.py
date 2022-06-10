53f6ba99261de2baa382885ecfd2e2710724fa70
sunbowen <550165764@qq.com>
2022-06-10 18:03:17 +0800

    delete useless code and black

--- RecommenderSystems/xdeepfm/xdeepfm_train_eval.py
+++ RecommenderSystems/xdeepfm/xdeepfm_train_eval.py
@@ -10,9 +10,7 @@ import oneflow as flow
 import oneflow.nn as nn
 from petastorm.reader import make_batch_reader
 
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
-)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 
 def get_args(print_args=True):
@@ -26,31 +24,19 @@ def get_args(print_args=True):
 
     parser.add_argument("--data_dir", type=str, required=True)
     parser.add_argument(
-        "--num_train_samples",
-        type=int,
-        required=True,
-        help="the number of train samples",
+        "--num_train_samples", type=int, required=True, help="the number of train samples",
     )
     parser.add_argument(
-        "--num_val_samples",
-        type=int,
-        required=True,
-        help="the number of validation samples",
+        "--num_val_samples", type=int, required=True, help="the number of validation samples",
     )
     parser.add_argument(
         "--num_test_samples", type=int, required=True, help="the number of test samples"
     )
 
+    parser.add_argument("--model_load_dir", type=str, default=None, help="model loading directory")
+    parser.add_argument("--model_save_dir", type=str, default=None, help="model saving directory")
     parser.add_argument(
-        "--model_load_dir", type=str, default=None, help="model loading directory"
-    )
-    parser.add_argument(
-        "--model_save_dir", type=str, default=None, help="model saving directory"
-    )
-    parser.add_argument(
-        "--save_initial_model",
-        action="store_true",
-        help="save initial model parameters or not",
+        "--save_initial_model", action="store_true", help="save initial model parameters or not",
     )
     parser.add_argument(
         "--save_model_after_each_eval",
@@ -58,9 +44,7 @@ def get_args(print_args=True):
         help="save model after each eval or not",
     )
 
-    parser.add_argument(
-        "--embedding_vec_size", type=int, default=16, help="embedding vector size"
-    )
+    parser.add_argument("--embedding_vec_size", type=int, default=16, help="embedding vector size")
     parser.add_argument(
         "--dnn_hidden_units",
         type=int_list,
@@ -68,29 +52,19 @@ def get_args(print_args=True):
         help="dnn hidden units number",
     )
     parser.add_argument(
-        "--cin_layer_units",
-        type=int_list,
-        default="16,16,16",
-        help="cin hidden units number",
+        "--cin_layer_units", type=int_list, default="16,16,16", help="cin hidden units number",
     )
-    parser.add_argument(
-        "--net_dropout", type=float, default=0.2, help="net dropout rate"
-    )
+    parser.add_argument("--net_dropout", type=float, default=0.2, help="net dropout rate")
 
     parser.add_argument("--lr_factor", type=float, default=0.1)
     parser.add_argument("--min_lr", type=float, default=1.0e-6)
-    parser.add_argument(
-        "--learning_rate", type=float, default=0.001, help="learning rate"
-    )
+    parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate")
 
     parser.add_argument(
         "--batch_size", type=int, default=10000, help="training/evaluation batch size"
     )
     parser.add_argument(
-        "--train_batches",
-        type=int,
-        default=75000,
-        help="the maximum number of training batches",
+        "--train_batches", type=int, default=75000, help="the maximum number of training batches",
     )
     parser.add_argument("--loss_print_interval", type=int, default=100, help="")
 
@@ -114,10 +88,7 @@ def get_args(print_args=True):
         required=True,
     )
     parser.add_argument(
-        "--persistent_path",
-        type=str,
-        required=True,
-        help="path for persistent kv store",
+        "--persistent_path", type=str, required=True, help="path for persistent kv store",
     )
     parser.add_argument(
         "--store_type",
@@ -133,20 +104,14 @@ def get_args(print_args=True):
     )
 
     parser.add_argument(
-        "--amp",
-        action="store_true",
-        help="enable Automatic Mixed Precision(AMP) training or not",
+        "--amp", action="store_true", help="enable Automatic Mixed Precision(AMP) training or not",
     )
-    parser.add_argument(
-        "--loss_scale_policy", type=str, default="static", help="static or dynamic"
-    )
+    parser.add_argument("--loss_scale_policy", type=str, default="static", help="static or dynamic")
 
     parser.add_argument(
         "--disable_early_stop", action="store_true", help="enable early stop or not"
     )
-    parser.add_argument(
-        "--save_best_model", action="store_true", help="save best model or not"
-    )
+    parser.add_argument("--save_best_model", action="store_true", help="save best model or not")
 
     args = parser.parse_args()
 
@@ -231,9 +196,7 @@ class xDeepFMDataReader(object):
                 pos = batch_size - len(tail[0])
                 tail = list(
                     [
-                        np.concatenate(
-                            (tail[i], rglist[i][0 : (batch_size - len(tail[i]))])
-                        )
+                        np.concatenate((tail[i], rglist[i][0 : (batch_size - len(tail[i]))]))
                         for i in range(self.num_fields)
                     ]
                 )
@@ -247,9 +210,7 @@ class xDeepFMDataReader(object):
                     continue
             while (pos + batch_size) <= len(rglist[0]):
                 label = rglist[0][pos : pos + batch_size]
-                features = [
-                    rglist[j][pos : pos + batch_size] for j in range(1, self.num_fields)
-                ]
+                features = [rglist[j][pos : pos + batch_size] for j in range(1, self.num_fields)]
                 pos += batch_size
                 yield label, np.stack(features, axis=-1)
             if pos != len(rglist[0]):
@@ -307,9 +268,7 @@ class OneEmbedding(nn.Module):
 
         if store_type == "device_mem":
             store_options = flow.one_embedding.make_device_mem_store_options(
-                persistent_path=persistent_path,
-                capacity=vocab_size,
-                size_factor=size_factor,
+                persistent_path=persistent_path, capacity=vocab_size, size_factor=size_factor,
             )
         elif store_type == "cached_host_mem":
             assert cache_memory_budget_mb > 0
@@ -346,12 +305,7 @@ class OneEmbedding(nn.Module):
 
 class DNN(nn.Module):
     def __init__(
-        self,
-        in_features,
-        hidden_units,
-        out_features,
-        skip_final_activation=False,
-        dropout=0.0,
+        self, in_features, hidden_units, out_features, skip_final_activation=False, dropout=0.0,
     ) -> None:
         super(DNN, self).__init__()
         denses = []
@@ -359,9 +313,7 @@ class DNN(nn.Module):
         use_relu = [True] * len(hidden_units) + [not skip_final_activation]
         hidden_units = [in_features] + hidden_units + [out_features]
         for idx in range(len(hidden_units) - 1):
-            denses.append(
-                nn.Linear(hidden_units[idx], hidden_units[idx + 1], bias=True)
-            )
+            denses.append(nn.Linear(hidden_units[idx], hidden_units[idx + 1], bias=True))
             if use_relu[idx]:
                 denses.append(nn.ReLU())
             if dropout_rates[idx] > 0:
@@ -378,13 +330,6 @@ class DNN(nn.Module):
         return self.linear_layers(x)
 
 
-def interaction(embedded_x: flow.Tensor) -> flow.Tensor:
-    sum_of_square = flow.sum(embedded_x, dim=1) ** 2
-    square_of_sum = flow.sum(embedded_x ** 2, dim=1)
-    bi_interaction = (sum_of_square - square_of_sum) * 0.5
-    return flow.sum(bi_interaction, dim=-1, keepdim=True)
-
-
 class CIN(nn.Module):
     def __init__(self, num_fields, cin_layer_units, output_dim=1):
         super(CIN, self).__init__()
@@ -392,18 +337,15 @@ class CIN(nn.Module):
         self.fc = nn.Linear(sum(cin_layer_units), output_dim)
         self.cin_layer = nn.ModuleDict()
         for i, unit in enumerate(self.cin_layer_units):
-            in_channels = (
-                num_fields * self.cin_layer_units[i - 1] if i > 0 else num_fields ** 2
-            )
+            in_channels = num_fields * self.cin_layer_units[i - 1] if i > 0 else num_fields ** 2
             out_channels = unit
             self.cin_layer["layer_" + str(i + 1)] = nn.Conv1d(
                 in_channels, out_channels, kernel_size=1  # how many filters
             )  # kernel output shape
 
-    def forward(self, feature_emb_list):
+    def forward(self, inputs):
         pooling_outputs = []
-        # X_0 = flow.stack(feature_emb_list, dim=1)  # b x field_size x emb_size
-        X_0 = feature_emb_list
+        X_0 = inputs
         batch_size = X_0.shape[0]
         embedding_dim = X_0.shape[-1]
         X_i = X_0
@@ -456,18 +398,16 @@ class xDeepFMModule(nn.Module):
             if dnn_hidden_units
             else None
         )
-        self.cin = CIN(
-            num_dense_fields + num_sparse_fields, cin_layer_units, output_dim=1
-        )
+        self.cin = CIN(num_dense_fields + num_sparse_fields, cin_layer_units, output_dim=1)
 
     def forward(self, inputs) -> flow.Tensor:
         multi_embedded_x = self.embedding_layer(inputs)
         embedded_x = multi_embedded_x[:, :, 0 : self.embedding_vec_size]
         lr_embedded_x = multi_embedded_x[:, :, -1]
 
-        # FM
+        # LR
         lr_logit = flow.sum(lr_embedded_x, dim=1, keepdim=True)
-
+        # CIN
         cin_logit = self.cin(embedded_x)
 
         if self.dnn_layer is not None:
@@ -475,10 +415,6 @@ class xDeepFMModule(nn.Module):
             y_pred = lr_logit + cin_logit + dnn_logit  # LR + CIN + DNN
         else:
             y_pred = lr_logit + cin_logit  # only LR + CIN
-
-        # # DNN
-        # dnn_pred = self.dnn_layer(embedded_x.flatten(start_dim=1))
-        # print("dnn_pred.shape: ", dnn_pred.shape)
 
         return y_pred
 
@@ -510,13 +446,7 @@ class xDeepFMValGraph(flow.nn.Graph):
 
 class xDeepFMTrainGraph(flow.nn.Graph):
     def __init__(
-        self,
-        xdeepfm_module,
-        loss,
-        optimizer,
-        grad_scaler=None,
-        amp=False,
-        lr_scheduler=None,
+        self, xdeepfm_module, loss, optimizer, grad_scaler=None, amp=False, lr_scheduler=None,
     ):
         super(xDeepFMTrainGraph, self).__init__()
         self.module = xdeepfm_module
@@ -540,9 +470,7 @@ def make_lr_scheduler(args, optimizer):
     batches_per_epoch = math.ceil(args.num_train_samples / args.batch_size)
     milestones = [
         batches_per_epoch * (i + 1)
-        for i in range(
-            math.floor(math.log(args.min_lr / args.learning_rate, args.lr_factor))
-        )
+        for i in range(math.floor(math.log(args.min_lr / args.learning_rate, args.lr_factor)))
     ]
     multistep_lr = flow.optim.lr_scheduler.MultiStepLR(
         optimizer=optimizer, milestones=milestones, gamma=args.lr_factor,
@@ -559,9 +487,7 @@ def get_metrics(logs):
     return monitor_value
 
 
-def early_stop(
-    epoch, monitor_value, best_metric, stopping_steps, patience=2, min_delta=1e-6
-):
+def early_stop(epoch, monitor_value, best_metric, stopping_steps, patience=2, min_delta=1e-6):
     rank = flow.env.get_rank()
     stop_training = False
     save_best = False
@@ -620,10 +546,7 @@ def train(args):
         grad_scaler = flow.amp.StaticGradScaler(1024)
     else:
         grad_scaler = flow.amp.GradScaler(
-            init_scale=1073741824,
-            growth_factor=2.0,
-            backoff_factor=0.5,
-            growth_interval=2000,
+            init_scale=1073741824, growth_factor=2.0, backoff_factor=0.5, growth_interval=2000,
         )
 
     eval_graph = xDeepFMValGraph(xdeepfm_module, args.amp)
@@ -639,9 +562,7 @@ def train(args):
     stop_training = False
 
     cached_eval_batches = prefetch_eval_batches(
-        f"{args.data_dir}/val",
-        args.batch_size,
-        math.ceil(args.num_val_samples / args.batch_size),
+        f"{args.data_dir}/val", args.batch_size, math.ceil(args.num_val_samples / args.batch_size),
     )
 
     xdeepfm_module.train()
@@ -753,15 +674,11 @@ def eval(args, eval_graph, tag="val", cur_step=0, epoch=0, cached_eval_batches=N
             preds.append(pred.to_local())
 
     labels = (
-        np_to_global(np.concatenate(labels, axis=0))
-        .to_global(sbp=flow.sbp.broadcast())
-        .to_local()
+        np_to_global(np.concatenate(labels, axis=0)).to_global(sbp=flow.sbp.broadcast()).to_local()
     )
     preds = (
         flow.cat(preds, dim=0)
-        .to_global(
-            placement=flow.env.all_device_placement("cpu"), sbp=flow.sbp.split(0)
-        )
+        .to_global(placement=flow.env.all_device_placement("cpu"), sbp=flow.sbp.split(0))
         .to_global(sbp=flow.sbp.broadcast())
         .to_local()
     )
@@ -773,9 +690,7 @@ def eval(args, eval_graph, tag="val", cur_step=0, epoch=0, cached_eval_batches=N
 
     metrics_start_time = time.time()
     auc = flow.roc_auc_score(labels, preds).numpy()[0]
-    logloss = flow._C.binary_cross_entropy_loss(
-        preds, labels, weight=None, reduction="mean"
-    )
+    logloss = flow._C.binary_cross_entropy_loss(preds, labels, weight=None, reduction="mean")
     metrics_time = time.time() - metrics_start_time
 
     if rank == 0:
