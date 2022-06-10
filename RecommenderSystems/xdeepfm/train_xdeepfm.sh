eab849d5e74fd95f5e5254dac8bf76f2d4fb237d
sunbowen <550165764@qq.com>
2022-06-10 17:38:18 +0800

    modify model name

--- RecommenderSystems/xdeepfm/train_xdeepfm.sh
+++ RecommenderSystems/xdeepfm/train_xdeepfm.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-rm -r /minio/sdb/sunbowen/persistent1/*
-
-export CUDA_VISIBLE_DEVICES=1
-export ONEFLOW_DEBUG_MODE=INFO
-export ONEFLOW_EAGER_LOCAL_TO_GLOBAL_BALANCED_OVERRIDE=1
-
 DEVICE_NUM_PER_NODE=1
 NUM_NODES=1
 NODE_RANK=0
 MASTER_ADDR=127.0.0.1
-DATA_DIR=/minio/sdb/sunbowen/criteo_x4_9ea3bdfc/pnn_parquet/new/deepfm_parquet_new
-PERSISTENT_PATH=/minio/sdb/sunbowen/persistent/persistent1
-MODEL_SAVE_DIR=/minio/sdb/sunbowen/saved_models/of_deepfm
+
+DATA_DIR=/path/to/xdeepfm_parquet
+PERSISTENT_PATH=/path/to/persistent
+MODEL_SAVE_DIR=/path/to/model/save/dir
+
 python3 -m oneflow.distributed.launch \
     --nproc_per_node $DEVICE_NUM_PER_NODE \
     --nnodes $NUM_NODES \
@@ -34,5 +30,4 @@ python3 -m oneflow.distributed.launch \
       --num_val_samples 4584062 \
       --num_test_samples 4584062 \
       --model_save_dir $MODEL_SAVE_DIR \
-      --save_best_model \
-    > run.log & tail -f run.log
+      --save_best_model
