7e29a06c1473b5f93d760a25cea854acfafb175d
Panlichen <myles.l.pan@gmail.com>
2022-06-21 18:58:32 +0800

    script

---
+++ Vision/classification/image/resnet50/examples/15_train_graph_distributed_fp32.sh
@@ -0,0 +1,75 @@
+# set -aux
+
+DEVICE_NUM_PER_NODE=4
+export ONEFLOW_TEST_DEVICE_NUM=4
+MASTER_ADDR=127.0.0.1
+NUM_NODES=1
+NODE_RANK=0
+
+export PYTHONUNBUFFERED=1
+echo PYTHONUNBUFFERED=$PYTHONUNBUFFERED
+export NCCL_LAUNCH_MODE=PARALLEL
+echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
+# export NCCL_DEBUG=INFO
+export ONEFLOW_DEBUG_MODE=1
+export ONEFLOW_PROFILER_KERNEL_PROFILE_KERNEL_FORWARD_RANGE=1
+export ONEFLOW_ENABLE_OFCCL=1
+
+CHECKPOINT_SAVE_PATH="./graph_distributed_fp32_checkpoints"
+if [ ! -d "$CHECKPOINT_SAVE_PATH" ]; then
+    mkdir $CHECKPOINT_SAVE_PATH
+fi
+
+OFRECORD_PATH=/dataset/ImageNet/ofrecord
+
+OFRECORD_PART_NUM=256
+LEARNING_RATE=0.768
+MOM=0.875
+EPOCH=50
+TRAIN_BATCH_SIZE=96
+VAL_BATCH_SIZE=50
+
+# SRC_DIR=/path/to/models/resnet50
+SRC_DIR=$(realpath $(dirname $0)/..)
+
+if [ $ONEFLOW_ENABLE_OFCCL == "1" ]; then
+    NSYS_FILE="ofccl_resnet"
+else
+    NSYS_FILE="nccl_resnet"
+fi
+
+rm -rf ./log
+mkdir ./log
+
+if [ -z $RUN_TYPE ];then
+    RUN_TYPE="NSYS"
+fi
+
+if [ "$RUN_TYPE" == "PURE" ];then
+    cmd="python3 -m oneflow.distributed.launch"
+elif [ "$RUN_TYPE" == "GDB" ];then
+    cmd="gdb -ex r --args python3 -m oneflow.distributed.launch"
+elif [ "$RUN_TYPE" == "NSYS" ];then
+    cmd="nsys profile -f true --trace=cuda,cudnn,cublas,osrt,nvtx -o nsys/$NSYS_FILE python3 -m oneflow.distributed.launch"
+fi
+
+$cmd \
+    --nproc_per_node $DEVICE_NUM_PER_NODE \
+    --nnodes $NUM_NODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    $SRC_DIR/train.py \
+        --save $CHECKPOINT_SAVE_PATH \
+        --ofrecord-path $OFRECORD_PATH \
+        --ofrecord-part-num $OFRECORD_PART_NUM \
+        --num-devices-per-node $DEVICE_NUM_PER_NODE \
+        --lr $LEARNING_RATE \
+        --momentum $MOM \
+        --num-epochs $EPOCH \
+        --train-batch-size $TRAIN_BATCH_SIZE \
+        --val-batch-size $VAL_BATCH_SIZE \
+        --use-gpu-decode \
+        --scale-grad \
+        --graph \
+        --fuse-bn-relu \
+        --fuse-bn-add-relu \
