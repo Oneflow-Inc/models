52dd1f2d351507981c4d1b2387bb45c74ef394cb
songzetao <925507966@qq.com>
2022-07-08 14:48:32 +0800

    copy as new PR

---
+++ NLP/roberta/roberta/infer_SST2.sh
@@ -0,0 +1,24 @@
+set -aux
+
+
+PRETRAIN_DIR="./roberta-base-oneflow/weights"
+KWARGS_PATH="./roberta-base-oneflow/parameters.json"
+MODEL_LOAD_DIR="./pretrain_model_SST-2"
+TEXT="this is junk food cinema at its greasiest ."  
+TASK="SST-2"
+# YOU CAN CHANGE THE 'TEXT' AS YOU WISH, SUCH AS /
+#"mark me down as a non-believer in werewolf films that are not serious and rely on stupidity as a substitute for humor ." 
+#"most new movies have a bright sheen ."
+
+if [ ! -d "$MODEL_LOAD_DIR" ]; then
+    echo "Directory '$MODEL_LOAD_DIR' doesn't exist."
+    echo "Please train the model first or download pretrained model."
+    exit 1
+fi
+
+python3 infer_SST2.py \
+    --pretrain_dir $PRETRAIN_DIR\
+    --kwargs_path $KWARGS_PATH\
+    --model_load_dir $MODEL_LOAD_DIR \
+    --text "$TEXT" \
+    --task $TASK
