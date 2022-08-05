3f46c112661d7ee4fb1755de4737df9ff66c68fd
ShawnXuan <xiexuanx2@gmail.com>
2022-08-05 16:47:27 +0800

    make_tabel_options

--- RecommenderSystems/pnn/pnn_train_eval.py
+++ RecommenderSystems/pnn/pnn_train_eval.py
@@ -242,7 +242,7 @@ class OneEmbedding(nn.Module):
         assert table_size_array is not None
         vocab_size = sum(table_size_array)
         tables = [
-            flow.one_embedding.make_table(
+            flow.one_embedding.make_table_options(
                 flow.one_embedding.make_normal_initializer(mean=0.0, std=1e-4)
             )
             for _ in range(len(table_size_array))
