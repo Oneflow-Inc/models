e551ac3ea15fe1bbf335c96381d41253ba14cbe7
songzetao <925507966@qq.com>
2022-08-09 11:07:22 +0800

    add infer.sh

--- NLP/KnowledgeDistillation/infer.sh
+++ NLP/KnowledgeDistillation/infer.sh
@@ -2,5 +2,5 @@ python infer.py \
     --model_type="student_kd" \
     --model_load_dir="./output/model_save/student_kd" \
     --image_save_name="./output/images/infer.jpg" \
-    --picture_index=1 \
+    --picture_index=9999 \
     --temperature=10
