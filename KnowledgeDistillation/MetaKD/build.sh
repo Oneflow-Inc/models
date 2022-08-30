cd6b729450fca8ec36e8ca6f66cf89bd2f2da915
songzetao <925507966@qq.com>
2022-08-30 13:53:50 +0800

    delete build.sh

--- KnowledgeDistillation/MetaKD/build.sh
+++
@@ -1,6 +0,0 @@
-pip install filelock
-pip install transformers
-pip install tensorboardx
-pip install datasets
-pip uninstall oneflow -y
-python3 -m pip install -f https://staging.oneflow.info/branch/master/cu112 --pre oneflow
